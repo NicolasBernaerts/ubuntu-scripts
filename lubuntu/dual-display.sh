402c6aac3f75f2d3bb14be2ea4a676a692ba11c3
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2014-08-11 20:44:56 +0200

    Delete dual-display.sh

--- lubuntu/dual-display.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
- 
-# arandr generated output
-xrandr --output LVDS1 --mode 1366x768 --pos 1920x312 --rotate normal --output VGA1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --off --output VIRTUAL1 --off --output DP1 --off
- 
-# lxpanel reload
-lxpanelctl restart
