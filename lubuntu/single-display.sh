f8a3d17d22201c8c5a74575d087512a64564d894
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2014-08-11 20:45:16 +0200

    Delete single-display.sh

--- lubuntu/single-display.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
- 
-# arandr generated output
-xrandr --output LVDS1 --mode 1366x768 --pos 0x0 --rotate normal --output VIRTUAL1 --off --output DP1 --off  --output HDMI1 --off --output VGA1 --off
- 
-# lxpanel reload
-lxpanelctl restart
