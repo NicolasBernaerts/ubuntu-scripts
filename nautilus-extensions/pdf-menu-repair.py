4f39298f06bbdc87a401072b055e4f2ecd42abeb
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2014-11-21 23:38:26 +0100

    Create pdf-menu-repair.py

---
+++ nautilus-extensions/pdf-menu-repair.py
@@ -0,0 +1,31 @@
+# ---------------------------------------------------
+# Nautilus extension to add a repair menu to PDF files 
+# Uses pdf-repair script
+#
+# Revision history :
+#   08/11/2014, V1.0 - Creation by N. Bernaerts
+# ---------------------------------------------------
+import subprocess
+from gi.repository import Nautilus, GObject
+
+class PDFCorrectionMenuProvider(GObject.GObject, Nautilus.MenuProvider):
+
+  def activate(self, menu, files):
+    # get document URI
+    filename = files[0].get_uri()
+
+    # run correction command
+    command_line = "pdf-repair " + filename
+    p = subprocess.Popen(command_line , shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+  def get_file_items(self, window, files):
+    # if multiple selection, nothing to do
+    if len(files) != 1: return
+
+    # if file is not a PDF document, nothing to do
+    if files[0].get_mime_type() != 'application/pdf': return
+
+    # create menu item for right click menu
+    item = Nautilus.MenuItem(name='SimpleMenuExtension::pdf_repair', label='Repair using Ghostscript')
+    item.connect('activate', self.activate, files)
+    return [item]
