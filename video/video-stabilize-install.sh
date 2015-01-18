a8cb8dd5bf058a3bee7e5a4fefb7b88ec6c1420d
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2015-01-18 19:28:32 +0100

    Update video-stabilize-install.sh

--- video/video-stabilize-install.sh
+++ video/video-stabilize-install.sh
@@ -2,17 +2,20 @@
 # Video rotation and stabilization
 
 # install melt, x264 and vidstab
-sudo add-apt-repository ppa:sunab/kdenlive-release
+sudo add-apt-repository -y ppa:sunab/kdenlive-release
 sudo apt-get update
-sudo apt-get install melt x264 libvidstab1.0
+sudo apt-get -y install melt x264 libvidstab1.0
 
 # install yad
-sudo add-apt-repository ppa:webupd8team/y-ppa-manager
+sudo add-apt-repository -y ppa:webupd8team/y-ppa-manager
 sudo apt-get update
-sudo apt-get install yad
+sudo apt-get -y install yad
 
 # install exiftool
-sudo apt-get install libimage-exiftool-perl
+sudo apt-get -y install libimage-exiftool-perl
+
+# install nautilus-actions
+sudo apt-get -y install nautilus-actions
 
 sudo wget -O /usr/local/bin/video-stabilize https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/video/video-stabilize
 sudo chmod +x /usr/local/bin/video-stabilize
