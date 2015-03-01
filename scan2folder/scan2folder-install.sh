09931c1a021788d91fef1d8a007519bb543a9cfb
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2015-03-01 22:15:04 +0100

    Update scan2folder-install.sh

--- scan2folder/scan2folder-install.sh
+++ scan2folder/scan2folder-install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # scan2folder tool
 
-sudo wget -O /usr/local/bin/scan2folder https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/scan2folder/scan2folder
+sudo wget -O /usr/local/bin/scan2folder https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/scanner/scan2folder
 sudo chmod +x /usr/local/bin/scan2folder
-sudo wget -O /usr/share/applications/scan2folder.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/scan2folder/scan2folder.desktop
+sudo wget -O /usr/share/applications/scan2folder.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/scanner/scan2folder.desktop
 sudo chmod +x /usr/share/applications/scan2folder.desktop
-wget -O $HOME/.scan2folder.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/scan2folder/scan2folder.conf
+wget -O $HOME/.scan2folder.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/scanner/scan2folder.conf
