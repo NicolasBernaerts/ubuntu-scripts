7998d57e66bdcb196d2506fc91ef2e851befe761
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2015-07-11 17:08:52 +0200

    Create pdf-generate-install.sh

---
+++ pdf/pdf-generate-install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# PDF generate extension
+
+sudo apt-get -y install imagemagick unoconv ghostscript zenity
+sudo wget -O /usr/local/bin/pdf-generate https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-generate
+sudo chmod +x /usr/local/bin/pdf-generate
+mkdir --parents $HOME/.local/share/file-manager/actions
+wget -O $HOME/.local/share/file-manager/actions/pdf-generate-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-generate-action.desktop
