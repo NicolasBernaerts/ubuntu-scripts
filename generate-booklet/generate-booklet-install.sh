76eac4b11df4e7025ed0e59bf1785657b3a4d87b
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2015-07-11 16:58:13 +0200

    Update and rename generate-booklet/generate-booklet-install.sh to pdf/generate-booklet-install.sh

--- generate-booklet/generate-booklet-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# PDF booklet generation extension
-
-sudo apt-get -y install poppler-utils pdfjam texlive-extra-utils
-sudo wget -O /usr/local/bin/generate-booklet https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/generate-booklet/generate-booklet
-sudo chmod +x /usr/local/bin/generate-booklet
-sudo wget -O /usr/share/applications/generate-booklet.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/generate-booklet/generate-booklet.desktop
-mkdir --parents $HOME/.local/share/file-manager/actions
-wget -O $HOME/.local/share/file-manager/actions/generate-booklet-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/generate-booklet/generate-booklet-action.desktop
