c3e65a4d46de2fbdea6a1f04f0e6580046c41500
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2015-07-21 08:04:17 +0200

    Update video-convert2mkv-install.sh

--- video/video-convert2mkv-install.sh
+++ video/video-convert2mkv-install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-# Convert any video file to MKV with AAC audio
+# Convert any video file to MKV with AAC audio and add midnight mode tracks
+
+# install yad
+sudo add-apt-repository -y ppa:webupd8team/y-ppa-manager
+sudo apt-get update
+sudo apt-get -y install yad
 
 # install mediainfo and mkvtoolnix
 sudo apt-get -y install mediainfo mkvtoolnix
