02fd16c0c14e4a33d73fcb795d5c60c835bff95e
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2015-07-29 05:46:14 +0200

    Update graphviz-thumbnailer-install.sh

--- thumbnailer/graphviz/graphviz-thumbnailer-install.sh
+++ thumbnailer/graphviz/graphviz-thumbnailer-install.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 # Graphviz thumbnailer
 
+# test Ubuntu distribution
+DISTRO=$(lsb_release -is 2>/dev/null)
+[ "${DISTRO}" != "Ubuntu" ] && { zenity --error --text="This automatic installation script is for Ubuntu only"; exit 1; }
+
+# install tools
 sudo apt-get -y install graphviz imagemagick eog
 
+# install previewer
 sudo wget -O /usr/share/icons/graphviz.png http://bernaerts.dyndns.org/download/ubuntu/graphviz/text-vnd.graphviz.png
 sudo wget -O /usr/local/bin/graphviz-preview https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/graphviz/graphviz-preview
 sudo chmod +x /usr/local/bin/graphviz-preview
+
+# desktop integration
 sudo wget -O /usr/share/applications/graphviz.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/graphviz/graphviz.desktop
 sudo chmod +x /usr/share/applications/graphviz.desktop
 
+# install main thumnailer script
 sudo wget -O /usr/local/bin/graphviz-thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/graphviz/graphviz-thumbnailer
 sudo chmod +x /usr/local/bin/graphviz-thumbnailer
+
+# thumbnailer integration
 sudo wget -O /usr/share/thumbnailers/graphviz.thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/graphviz/graphviz.thumbnailer
