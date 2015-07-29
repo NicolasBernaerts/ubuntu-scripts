dbe2e214ee829c0472e2c4844957957b097ae3b3
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2015-07-29 05:41:42 +0200

    Update gimp-tumbnailer-install.sh

--- thumbnailer/gimp/gimp-tumbnailer-install.sh
+++ thumbnailer/gimp/gimp-tumbnailer-install.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 # GIMP XCF thumbnailer
 
+# test Ubuntu distribution
+DISTRO=$(lsb_release -is 2>/dev/null)
+[ "${DISTRO}" != "Ubuntu" ] && { zenity --error --text="This automatic installation script is for Ubuntu only"; exit 1; }
+
+# install tools
 sudo apt-get -y install gvfs-bin imagemagick xcftools
+
+# install main script
 sudo wget -O /usr/local/sbin/gimp-thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/gimp/gimp-thumbnailer
 sudo chmod +x /usr/local/sbin/gimp-thumbnailer
+
+# thumbnailer integration
 sudo wget -O /usr/share/thumbnailers/gimp.thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/gimp/gimp.thumbnailer
