b9c92847b64cc392e1a7b4feddff1bc88ccc6cbb
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2015-07-29 05:36:16 +0200

    Update generate-booklet-install.sh

--- pdf/generate-booklet-install.sh
+++ pdf/generate-booklet-install.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 # PDF booklet generation extension
 
+# test Ubuntu distribution
+DISTRO=$(lsb_release -is 2>/dev/null)
+[ "${DISTRO}" != "Ubuntu" ] && { zenity --error --text="This automatic installation script is for Ubuntu only"; exit 1; }
+
+# install tools
 sudo apt-get -y install poppler-utils texlive-extra-utils
+
+# install main script
 sudo wget -O /usr/local/bin/generate-booklet https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/generate-booklet
 sudo chmod +x /usr/local/bin/generate-booklet
+
+# desktop integration
 sudo wget -O /usr/share/applications/generate-booklet.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/generate-booklet.desktop
 mkdir --parents $HOME/.local/share/file-manager/actions
 wget -O $HOME/.local/share/file-manager/actions/generate-booklet-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/generate-booklet-action.desktop
