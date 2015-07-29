73df437b603dafe489c615fbeb9381227e30f87e
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2015-07-29 05:44:12 +0200

    Create apk-thumbnailer-install.sh

---
+++ thumbnailer/apk/apk-thumbnailer-install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# APK thumbnailer
+
+# test Ubuntu distribution
+DISTRO=$(lsb_release -is 2>/dev/null)
+[ "${DISTRO}" != "Ubuntu" ] && { zenity --error --text="This automatic installation script is for Ubuntu only"; exit 1; }
+
+# install tools
+sudo apt-get -y install libfile-mimeinfo-perl gvfs-bin unzip imagemagick python-nautilus
+
+# install aapt
+wget http://bernaerts.dyndns.org/download/ubuntu/android-tools-aapt_4.2.2_$ARCHI.deb
+sudo dpkg -i android-tools-aapt_*.deb
+
+# install main script
+sudo wget -O /usr/local/sbin/apk-thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/apk-thumbnailer/apk-thumbnailer
+sudo chmod +x /usr/local/sbin/apk-thumbnailer
+
+# thumbnailer integration
+sudo wget -O /usr/share/thumbnailers/apk.thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/apk-thumbnailer/apk.thumbnailer
