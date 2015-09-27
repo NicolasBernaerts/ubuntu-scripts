9c6a6d2d852e34b8decd8a59cf0312ea0401732d
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2015-09-27 22:20:58 +0200

    Update pdf-generate-install.sh

--- pdf/pdf-generate-install.sh
+++ pdf/pdf-generate-install.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 # PDF generate extension
 
+# if nautilus present, install nautilus-actions
+command -v nautilus >/dev/null 2>&1 && sudo apt-get -y install nautilus-actions
+
+# install configuration file 
+mkdir --parents $HOME/.config
+wget -O $HOME/.config/pdf-generate.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-generate.conf
+
+# main packages installation
 sudo apt-get -y install imagemagick unoconv ghostscript zenity libfile-mimeinfo-perl
+
+# main script installation
 sudo wget -O /usr/local/bin/pdf-generate https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-generate
 sudo chmod +x /usr/local/bin/pdf-generate
+
+# desktop integration
 mkdir --parents $HOME/.local/share/file-manager/actions
 wget -O $HOME/.local/share/file-manager/actions/pdf-generate-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-generate-action.desktop
