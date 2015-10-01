760ace526cf3443cd8061d4b51de997aa1b38b93
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2015-10-01 19:58:35 +0200

    Update dual-display-install.sh

--- lubuntu/dual-display/dual-display-install.sh
+++ lubuntu/dual-display/dual-display-install.sh
@@ -2,9 +2,9 @@
  
 # place scripts to handle display change
 mkdir -p $HOME/.screenlayout
-wget -O $HOME/.screenlayout/single-display.sh https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/lubuntu/dual-display/single-display.sh
-wget -O $HOME/.screenlayout/dual-display.sh https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/lubuntu/dual-display/dual-display.sh
+wget --header='Accept-Encoding:none' -O $HOME/.screenlayout/single-display.sh https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/lubuntu/dual-display/single-display.sh
+wget --header='Accept-Encoding:none' -O $HOME/.screenlayout/dual-display.sh https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/lubuntu/dual-display/dual-display.sh
  
 # declare keyboard shortcut
-wget https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/lubuntu/dual-display/dual-display.xml
+wget --header='Accept-Encoding:none' https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/lubuntu/dual-display/dual-display.xml
 sed -i -e '/<!-- Keybindings for desktop switching -->/r dual-display.xml' $HOME/.config/openbox/lubuntu-rc.xml
