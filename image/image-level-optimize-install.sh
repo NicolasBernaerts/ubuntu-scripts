481bd64016fe69c02eee80aa5312961638992010
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2015-10-01 19:56:18 +0200

    Update image-level-optimize-install.sh

--- image/image-level-optimize-install.sh
+++ image/image-level-optimize-install.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 # Image levels optimize extension
 
+# install packages
 sudo apt-get -y install imagemagick
-sudo wget -O /usr/local/bin/image-level-optimize https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/image/image-level-optimize
+
+# main script
+sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/image-level-optimize https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/image/image-level-optimize
 sudo chmod +x /usr/local/bin/image-level-optimize
-sudo wget -O /usr/share/icons/image-level-optimize.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/image/image-level-optimize.png
-sudo wget -O /usr/share/applications/image-level-optimize.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/image/image-level-optimize.desktop
+
+# desktop integration
+sudo wget --header='Accept-Encoding:none' -O /usr/share/icons/image-level-optimize.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/image/image-level-optimize.png
+sudo wget --header='Accept-Encoding:none' -O /usr/share/applications/image-level-optimize.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/image/image-level-optimize.desktop
+
+# nautilus action
 mkdir --parents $HOME/.local/share/file-manager/actions
-wget -O $HOME/.local/share/file-manager/actions/image-level-optimize-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/image/image-level-optimize-action.desktop
+wget --header='Accept-Encoding:none' -O $HOME/.local/share/file-manager/actions/image-level-optimize-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/image/image-level-optimize-action.desktop
