cf32fd556225ee868a03e7add083f3aac81cad21
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2015-10-01 19:57:54 +0200

    Update image-resize-install.sh

--- image/image-resize-install.sh
+++ image/image-resize-install.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 # Image resize extension
 
+# install packages
 sudo apt-get -y install imagemagick
-wget -O $HOME/.image-resize.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/image/image-resize.conf
-sudo wget -O /usr/local/bin/image-resize https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/image/image-resize
+
+# main script
+wget --header='Accept-Encoding:none' -O $HOME/.image-resize.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/image/image-resize.conf
+sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/image-resize https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/image/image-resize
 sudo chmod +x /usr/local/bin/image-resize
-sudo wget -O /usr/share/applications/image-resize.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/image/image-resize.desktop
+
+# desktop integration
+sudo wget --header='Accept-Encoding:none' -O /usr/share/applications/image-resize.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/image/image-resize.desktop
+
+# nautilus action
 mkdir --parents $HOME/.local/share/file-manager/actions
-wget -O $HOME/.local/share/file-manager/actions/image-resize-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/image/image-resize-action.desktop
+wget --header='Accept-Encoding:none' -O $HOME/.local/share/file-manager/actions/image-resize-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/image/image-resize-action.desktop
