0c248a17863ee52c5d6ebc28ce5e7d69e94f7f95
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2015-10-02 07:23:49 +0200

    Update android-screenshot-install.sh

--- android/android-screenshot-install.sh
+++ android/android-screenshot-install.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 # Android device screenshot utility
 
+# install packages
 sudo apt-get -y install yad avconv android-tools-adb
-sudo wget -O /usr/local/bin/android-screenshot https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/android/android-screenshot
+
+# main script
+sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/android-screenshot https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/android/android-screenshot
 sudo chmod +x /usr/local/bin/android-screenshot
-sudo wget -O /usr/share/applications/android-screenshot.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/android/android-screenshot.desktop
-wget -O $HOME/.android-screenshot.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/android/android-screenshot.conf
+
+# configuration file
+wget --header='Accept-Encoding:none' -O $HOME/.android-screenshot.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/android/android-screenshot.conf
+
+# desktop integration
+sudo wget --header='Accept-Encoding:none' -O /usr/share/applications/android-screenshot.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/android/android-screenshot.desktop
