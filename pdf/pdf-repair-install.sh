136e7d0422a66a9a2fbc227e904d84aed7a55810
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2015-10-02 07:05:25 +0200

    Update pdf-repair-install.sh

--- pdf/pdf-repair-install.sh
+++ pdf/pdf-repair-install.sh
@@ -9,10 +9,10 @@ DISTRO=$(lsb_release -is 2>/dev/null)
 sudo apt-get -y install gvfs-bin libnotify-bin ghostscript zenity
 
 # install main script
-sudo wget -O /usr/local/bin/pdf-repair https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-repair
+sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/pdf-repair https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-repair
 sudo chmod +x /usr/local/bin/pdf-repair
 
 # desktop integration
-sudo wget -O /usr/share/applications/pdf-repair.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-repair.desktop
+sudo wget --header='Accept-Encoding:none' -O /usr/share/applications/pdf-repair.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-repair.desktop
 mkdir --parents $HOME/.local/share/file-manager/actions
-wget -O $HOME/.local/share/file-manager/actions/pdf-repair-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-repair-action.desktop
+wget --header='Accept-Encoding:none' -O $HOME/.local/share/file-manager/actions/pdf-repair-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-repair-action.desktop
