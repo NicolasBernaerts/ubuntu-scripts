b898424999f2ab2ae6d7ba8f3d5855884685dec2
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2015-10-02 07:04:38 +0200

    Update pdf-generate-install.sh

--- pdf/pdf-generate-install.sh
+++ pdf/pdf-generate-install.sh
@@ -6,16 +6,16 @@ command -v nautilus >/dev/null 2>&1 && sudo apt-get -y install nautilus-actions
 
 # install configuration file 
 mkdir --parents $HOME/.config
-wget -O $HOME/.config/pdf-generate.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-generate.conf
+wget --header='Accept-Encoding:none' -O $HOME/.config/pdf-generate.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-generate.conf
 
 # main packages installation
 sudo apt-get -y install imagemagick unoconv ghostscript zenity libfile-mimeinfo-perl
 
 # main script installation
-sudo wget -O /usr/local/bin/pdf-generate https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-generate
+sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/pdf-generate https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-generate
 sudo chmod +x /usr/local/bin/pdf-generate
 
 # desktop integration
-sudo wget -O /usr/share/applications/pdf-generate.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-generate.desktop
+sudo wget --header='Accept-Encoding:none' -O /usr/share/applications/pdf-generate.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-generate.desktop
 mkdir --parents $HOME/.local/share/file-manager/actions
-wget -O $HOME/.local/share/file-manager/actions/pdf-generate-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-generate-action.desktop
+wget --header='Accept-Encoding:none' -O $HOME/.local/share/file-manager/actions/pdf-generate-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-generate-action.desktop
