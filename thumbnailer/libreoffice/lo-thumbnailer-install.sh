308b9ef485a5bcaae60045079f5e529a917cfc47
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2015-10-02 07:14:53 +0200

    Update lo-thumbnailer-install.sh

--- thumbnailer/libreoffice/lo-thumbnailer-install.sh
+++ thumbnailer/libreoffice/lo-thumbnailer-install.sh
@@ -9,13 +9,13 @@ DISTRO=$(lsb_release -is 2>/dev/null)
 sudo apt-get -y install libfile-mimeinfo-perl gvfs-bin unzip imagemagick
 
 # install thumbnailer helper files
-wget http://bernaerts.dyndns.org/download/gnome/thumbnailer/lo-thumbnailer-icons.zip
+wget --header='Accept-Encoding:none' http://bernaerts.dyndns.org/download/gnome/thumbnailer/lo-thumbnailer-icons.zip
 sudo unzip -d /usr/local/sbin lo-thumbnailer-icons.zip
 rm lo-thumbnailer-icons.zip
 
 # install main script
-sudo wget -O /usr/local/sbin/lo-thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/lo-thumbnailer
+sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/lo-thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/lo-thumbnailer
 sudo chmod +rx /usr/local/sbin/lo-thumbnailer
 
 # thumbnailer integration
-sudo wget -O /usr/share/thumbnailers/lo.thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/lo.thumbnailer
+sudo wget --header='Accept-Encoding:none' -O /usr/share/thumbnailers/lo.thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/lo.thumbnailer
