354f02a21810b5831388664ef2637679e1b5f6e7
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2015-10-02 07:08:21 +0200

    Update envelope2address-install.sh

--- scanner/envelope2address-install.sh
+++ scanner/envelope2address-install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # install tool to scan envelopes and OCR addresses
-sudo wget -O /usr/local/bin/envelope2address https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/scanner/envelope2address
+
+# main script
+sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/envelope2address https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/scanner/envelope2address
 sudo chmod +x /usr/local/bin/envelope2address
-sudo wget -O /usr/share/applications/envelope2address.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/scanner/envelope2address.desktop
+
+# desktop integration
+sudo wget --header='Accept-Encoding:none' -O /usr/share/applications/envelope2address.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/scanner/envelope2address.desktop
 sudo chmod +x /usr/share/applications/envelope2address.desktop
