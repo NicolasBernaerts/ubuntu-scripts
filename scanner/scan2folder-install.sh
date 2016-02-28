5861a8bf963b5f8e6092e7758fa38fccea1632b9
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2016-02-28 21:43:34 +0100

    Update scan2folder-install.sh

--- scanner/scan2folder-install.sh
+++ scanner/scan2folder-install.sh
@@ -4,6 +4,15 @@
 # install tools
 sudo apt-get -y install ghostscript zenity
 
+# install yad
+IS_PRESENT=$(command -v yad)
+if [ -z "${IS_PRESENT}" ]
+then
+  sudo add-apt-repository -y ppa:webupd8team/y-ppa-manager
+  sudo apt-get update
+  sudo apt-get -y install yad
+fi
+
 # main script
 sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/scan2folder https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/scanner/scan2folder
 sudo chmod +x /usr/local/bin/scan2folder
