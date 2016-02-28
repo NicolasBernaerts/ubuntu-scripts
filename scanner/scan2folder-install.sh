4b3d0d3016d9e653fa231eac10b1534f3b6b9b40
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2016-02-28 21:42:43 +0100

    Update scan2folder-install.sh

--- scanner/scan2folder-install.sh
+++ scanner/scan2folder-install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # scan2folder tool
 
+# install tools
+sudo apt-get -y install ghostscript zenity
+
 # main script
 sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/scan2folder https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/scanner/scan2folder
 sudo chmod +x /usr/local/bin/scan2folder
