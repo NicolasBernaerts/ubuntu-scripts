7784aa9dc05b062e3ea6a5238c2ae5116505174c
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2016-02-28 21:41:43 +0100

    Update scan2folder-install.sh

--- scanner/scan2folder-install.sh
+++ scanner/scan2folder-install.sh
@@ -5,8 +5,9 @@
 sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/scan2folder https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/scanner/scan2folder
 sudo chmod +x /usr/local/bin/scan2folder
 
-# configuration file 
-wget --header='Accept-Encoding:none' -O $HOME/.scan2folder.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/scanner/scan2folder.conf
+# configuration file
+mkdir --parent $HOME/.config
+wget --header='Accept-Encoding:none' -O $HOME/.config/scan2folder.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/scanner/scan2folder.conf
 
 # desktop integration
 sudo wget --header='Accept-Encoding:none' -O /usr/share/applications/scan2folder.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/scanner/scan2folder.desktop
