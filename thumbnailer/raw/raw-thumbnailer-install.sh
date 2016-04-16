3f6ff733adccb89a4e01409b71c1a3b9a280fe86
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2016-04-16 02:22:12 +0200

    Create raw-thumbnailer-install.sh

---
+++ thumbnailer/raw/raw-thumbnailer-install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# APK thumbnailer
+
+# install ufraw-batch
+sudo apt-get install ufraw-batch
+
+# thumbnailer integration
+sudo wget --header='Accept-Encoding:none' -O /usr/share/thumbnailers/raw.thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/raw/raw.thumbnailer
