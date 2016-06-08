851c8e2d4467f429a3dcc77b3359e00f989b0bda
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2016-06-09 00:56:37 +0200

    Create New Bash Script.sh

---
+++ nautilus/newfile/New Bash Script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# ---------------------------------------------------
+# New bash script
+#
+# Parameters :
+#
+# Revision history :
+#   xx/xx/xxxx, V1.0 - Creation by 
+# ---------------------------------------------------
+
