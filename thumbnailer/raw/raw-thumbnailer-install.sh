f42db578d95744b14850ce0d7359a4f09a81cab1
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2016-08-14 14:03:06 +0200

    Use new raw-thumbnailer script

--- thumbnailer/raw/raw-thumbnailer-install.sh
+++ thumbnailer/raw/raw-thumbnailer-install.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 # RAW files thumbnailer
 
-# install ufraw-batch
-sudo apt-get install ufraw-batch
+# install tools
+sudo apt-get -y install dcraw imagemagick
+
+# install main script
+sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/raw-thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/raw/raw-thumbnailer
+sudo chmod +x /usr/local/sbin/raw-thumbnailer
 
 # thumbnailer integration
 sudo wget --header='Accept-Encoding:none' -O /usr/share/thumbnailers/raw.thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/raw/raw.thumbnailer
