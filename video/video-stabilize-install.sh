3bc0b16c23cebe90229e252e1c514bab7855d356
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2016-08-28 03:56:50 +0200

    Update video-stabilize-install.sh

--- video/video-stabilize-install.sh
+++ video/video-stabilize-install.sh
@@ -1,33 +1,17 @@
 #!/usr/bin/env bash
 # Video rotation and stabilization
+# This version is meant for Xenial 16.04, it is not compatible anymore with 14.04
 
 # test Ubuntu distribution
 DISTRO=$(lsb_release -is 2>/dev/null)
 [ "${DISTRO}" != "Ubuntu" ] && { zenity --error --text="This automatic installation script is for Ubuntu only"; exit 1; }
 
-# declare sunab/kdenlive-release ppa if not declared
-PPA_NAME="sunab/kdenlive-release"
-IS_PRESENT=$(grep "^[^#].*${PPA_NAME}.*" /etc/apt/sources.list /etc/apt/sources.list.d/*)
-if [ -z "${IS_PRESENT}" ]
-then
-  sudo add-apt-repository -y ppa:sunab/kdenlive-release
-fi
-
-# install melt and libvidstab
+# install melt from official repository (without vid.stab filter)
 sudo apt-get update
-sudo apt-get -y install libvidstab1.0 melt
+sudo apt-get -y install melt
 
-# install yad
-IS_PRESENT=$(command -v yad)
-if [ -z "${IS_PRESENT}" ]
-then
-  sudo add-apt-repository -y ppa:webupd8team/y-ppa-manager
-  sudo apt-get update
-  sudo apt-get -y install yad
-fi
-
-# install x264 and exiftool
-sudo apt-get -y install x264 libimage-exiftool-perl
+# install yad, x264 and exiftool
+sudo apt-get -y install yad x264 libimage-exiftool-perl
 
 # install opencv-data, to avoid error
 #   Could not load classifier cascade /usr/share/opencv/haarcascades/haarcascade_frontalface_alt2.xml
@@ -35,6 +19,13 @@ sudo apt-get -y install opencv-data
 
 # if nautilus present, install nautilus-actions
 command -v nautilus >/dev/null 2>&1 && sudo apt-get -y install nautilus-actions
+
+# If not present, install specific version of melt with vid.stab filter
+if [ ! -f /opt/mlt-vidstab/melt ]
+then
+  wget --header='Accept-Encoding:none' https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/video/melt-vidstab-install.sh
+  . ./melt-vidstab-install.sh
+fi
 
 # install configuration file
 mkdir --parents $HOME/.config
@@ -44,8 +35,12 @@ wget --header='Accept-Encoding:none' -O $HOME/.config/video-stabilize.conf https
 sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/video-stabilize https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/video/video-stabilize
 sudo chmod +x /usr/local/bin/video-stabilize
 
-# desktop integration
+# desktop integration : icon
 sudo wget --header='Accept-Encoding:none' -O /usr/share/icons/stabilizer.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/video/stabilizer.png
+
+# desktop integration : application
 sudo wget --header='Accept-Encoding:none' -O /usr/share/applications/video-stabilize.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/video/video-stabilize.desktop
+
+# desktop integration : nautilus action
 mkdir --parents $HOME/.local/share/file-manager/actions
 wget --header='Accept-Encoding:none' -O $HOME/.local/share/file-manager/actions/video-stabilize-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/video/video-stabilize-action.desktop
