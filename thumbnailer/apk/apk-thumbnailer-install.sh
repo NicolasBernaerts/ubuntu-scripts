d269e6cae16695d0adb9e2ab7281bdd7a9c9da58
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2016-08-31 17:05:01 +0200

    Install aapt thru official repository

--- thumbnailer/apk/apk-thumbnailer-install.sh
+++ thumbnailer/apk/apk-thumbnailer-install.sh
@@ -9,8 +9,7 @@ DISTRO=$(lsb_release -is 2>/dev/null)
 sudo apt-get -y install libfile-mimeinfo-perl gvfs-bin unzip imagemagick python-nautilus
 
 # install aapt
-wget --header='Accept-Encoding:none' http://bernaerts.dyndns.org/download/ubuntu/android-tools-aapt_4.2.2_$ARCHI.deb
-sudo dpkg -i android-tools-aapt_*.deb
+sudo apt-get -y install aapt
 
 # install main script
 sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/apk-thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/apk/apk-thumbnailer
