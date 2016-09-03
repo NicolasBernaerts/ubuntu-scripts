5df83b25f1d1c95df4fe3faf618647ffc93885a2
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2016-09-03 02:53:22 +0200

    Update youtubedl-install.sh

--- video/youtubedl-install.sh
+++ video/youtubedl-install.sh
@@ -5,18 +5,9 @@
 DISTRO=$(lsb_release -is 2>/dev/null)
 [ "${DISTRO}" != "Ubuntu" ] && { zenity --error --text="This automatic installation script is for Ubuntu only"; exit 1; }
 
-# install yad
-IS_PRESENT=$(command -v yad)
-if [ -z "${IS_PRESENT}" ]
-then
-  sudo add-apt-repository -y ppa:webupd8team/y-ppa-manager
-  sudo apt-get update
-  sudo apt-get -y install yad
-fi
-
-# install youtube-dl and mkvmerge
+# install youtube-dl, mkvmerge and yad
 sudo apt-get update
-sudo apt-get -y install youtube-dl mkvtoolnix
+sudo apt-get -y install youtube-dl mkvtoolnix yad
 
 # install main script
 sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/youtubedl-gui https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/video/youtubedl-gui
