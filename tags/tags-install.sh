a0c8d2a3cea0dae88c83333391c5bfc82497999b
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2016-09-10 15:24:51 +0200

    Update tags-install.sh

--- tags/tags-install.sh
+++ tags/tags-install.sh
@@ -9,27 +9,17 @@ DISTRO=$(lsb_release -is 2>/dev/null)
 sudo apt-get -y install libimage-exiftool-perl imagemagick
 
 # install yad
-IS_PRESENT=$(command -v yad)
-if [ -z "${IS_PRESENT}" ]
-then
-  sudo add-apt-repository -y ppa:webupd8team/y-ppa-manager
-  sudo apt-get update
-  sudo apt-get -y install yad
-fi
+sudo apt-get -y install yad
 
 # install configuration file
 mkdir --parents $HOME/.config
-wget --header='Accept-Encoding:none' -O $HOME/.config/tags-xmp.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/tags/tags-xmp.conf
+wget --header='Accept-Encoding:none' -O $HOME/.config/tags-update.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/tags/tags-update.conf
 
 # install main script
-sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/tags-common https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/tags/tags-common
-sudo chmod +x /usr/local/bin/tags-common
-sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/tags-xmp https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/tags/tags-xmp
-sudo chmod +x /usr/local/bin/tags-xmp
-sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/tags-timeshift https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/tags/tags-timeshift
-sudo chmod +x /usr/local/bin/tags-timeshift
-sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/tags-creationdate https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/tags/tags-creationdate
-sudo chmod +x /usr/local/bin/tags-creationdate
+sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/tags-update https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/tags/tags-update
+sudo chmod +x /usr/local/bin/tags-update
+sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/tags-date https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/tags/tags-date
+sudo chmod +x /usr/local/bin/tags-date
 
 # desktop integration : icon
 sudo wget --header='Accept-Encoding:none' -O /usr/share/icons/tags.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/tags/tags.png
@@ -42,7 +32,7 @@ rm $HOME/.local/share/file-manager/actions/tags-*.desktop
 wget -O $HOME/.local/share/file-manager/actions/tags-menu.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/tags/tags-menu.desktop
 
 # desktop integration : entries
-wget -O $HOME/.local/share/file-manager/actions/tags-xmp-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/tags/tags-xmp-action.desktop
-wget -O $HOME/.local/share/file-manager/actions/tags-timeshift-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/tags/tags-timeshift-action.desktop
-wget -O $HOME/.local/share/file-manager/actions/tags-creationdate-rename.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/tags/tags-creationdate-rename.desktop
-wget -O $HOME/.local/share/file-manager/actions/tags-creationdate-system.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/tags/tags-creationdate-system.desktop
+wget -O $HOME/.local/share/file-manager/actions/tags-update-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/tags/tags-update-action.desktop
+wget -O $HOME/.local/share/file-manager/actions/tags-date-shift.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/tags/tags-date-shift.desktop
+wget -O $HOME/.local/share/file-manager/actions/tags-date-rename.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/tags/tags-date-rename.desktop
+wget -O $HOME/.local/share/file-manager/actions/tags-date-system.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/tags/tags-date-system.desktop
