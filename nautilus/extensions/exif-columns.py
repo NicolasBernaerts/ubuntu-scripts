4b9be82c024b5b01ac99ed9f5d1d2b96f56670ad
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2016-09-11 12:07:14 +0200

    Update exif-columns.py

--- nautilus/extensions/exif-columns.py
+++ nautilus/extensions/exif-columns.py
@@ -3,8 +3,7 @@
 # Nautilus extension to add EXIF specific columns
 # Procedure :
 #   http://bernaerts.dyndns.org/linux/xxx...
-# Depends on :
-#   * 
+#
 # Revision history :
 #   20/09/2016, V1.0 - Creation by N. Bernaerts
 # ---------------------------------------------------
@@ -41,12 +40,18 @@ class ExifColumnExtension(GObject.GObject, Nautilus.ColumnProvider, Nautilus.Inf
     if 'Xmp.photoshop.City' in self.tags: tag_city = self.tags['Xmp.photoshop.City']
     if 'Xmp.photoshop.Country' in self.tags: tag_country = self.tags['Xmp.photoshop.Country']
 
-    return tag_camera, tag_focal, tag_city, tag_country
+    # check if GPS info available
+    posGPS = self.tags.get_gps_info()
+    if posGPS[0] <> 0 or posGPS[1] <> 0: tag_gps = "yes"
+    else: tag_gps = "no"
+
+    return tag_camera, tag_focal, tag_gps, tag_city, tag_country
 
   def get_columns(self):
       return (
         Nautilus.Column(name="NautilusPython::ExifCameraType", attribute="exif_camera_type", label="Camera", description="Camera model"),
         Nautilus.Column(name="NautilusPython::ExifFocalLength", attribute="exif_focal_length", label="Focal length", description="Focal length (equiv. 35mm)"),
+        Nautilus.Column(name="NautilusPython::ExifGPS", attribute="exif_gps", label="GPS", description="Has GPS tags"),
         Nautilus.Column(name="NautilusPython::XMPCity", attribute="xmp_city", label="City", description="City (XMP tag)"),
         Nautilus.Column(name="NautilusPython::XMPCountry", attribute="xmp_country", label="Country", description="Country (XMP tag)"),
       )
@@ -62,10 +67,11 @@ class ExifColumnExtension(GObject.GObject, Nautilus.ColumnProvider, Nautilus.Inf
       filename = urllib.unquote(file.get_uri()[7:])
       result = self.get_data(filename)
     else:
-      result=("", "", "", "") 
+      result=("-", "-", "-", "-", "-") 
 
     # add data to file attributes
     file.add_string_attribute('exif_camera_type', result[0])
     file.add_string_attribute('exif_focal_length', result[1])
-    file.add_string_attribute('xmp_city', result[2])
-    file.add_string_attribute('xmp_country', result[3])
+    file.add_string_attribute('exif_gps', result[2])
+    file.add_string_attribute('xmp_city', result[3])
+    file.add_string_attribute('xmp_country', result[4])
