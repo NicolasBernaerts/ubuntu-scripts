6362b0ef5cbf452f7a3f8ce68401db91c650cdd8
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2016-09-29 19:13:32 +0200

    Update melt-vidstab-install.sh

--- video/melt-vidstab-install.sh
+++ video/melt-vidstab-install.sh
@@ -11,24 +11,29 @@ sudo mkdir --parent /opt/vidstab
 # extract file from melt
 wget https://launchpad.net/~sunab/+archive/ubuntu/kdenlive-release/+build/9289708/+files/melt_6.0.0-0ubuntu0~sunab~xenial1_amd64.deb
 dpkg --fsys-tarfile melt_6.0.0-0ubuntu0~sunab~xenial1_amd64.deb | sudo tar xf - -C /opt/vidstab
+rm melt_6.0.0-0ubuntu0~sunab~xenial1_amd64.deb
 
 # extract files from libmlt6
 wget https://launchpad.net/~sunab/+archive/ubuntu/kdenlive-release/+build/9289708/+files/libmlt6_6.0.0-0ubuntu0~sunab~xenial1_amd64.deb
 dpkg --fsys-tarfile libmlt6_6.0.0-0ubuntu0~sunab~xenial1_amd64.deb | sudo tar xf - -C /opt/vidstab
 sudo ln -s /opt/vidstab/usr/lib/mlt /usr/lib/mlt
+rm libmlt6_6.0.0-0ubuntu0~sunab~xenial1_amd64.deb
 
 # extract file from libmlt++3
 wget https://launchpad.net/~sunab/+archive/ubuntu/kdenlive-release/+build/9289708/+files/libmlt++3_6.0.0-0ubuntu0~sunab~xenial1_amd64.deb
 dpkg --fsys-tarfile libmlt++3_6.0.0-0ubuntu0~sunab~xenial1_amd64.deb | sudo tar xf - -C /opt/vidstab
+rm libmlt++3_6.0.0-0ubuntu0~sunab~xenial1_amd64.deb
 
 # extract files from libmlt-data
 wget https://launchpad.net/~sunab/+archive/ubuntu/kdenlive-release/+build/9289708/+files/libmlt-data_6.0.0-0ubuntu0~sunab~xenial1_all.deb
 dpkg --fsys-tarfile libmlt-data_6.0.0-0ubuntu0~sunab~xenial1_all.deb | sudo tar xf - -C /opt/vidstab
 sudo ln -s /opt/vidstab/usr/share/mlt/vid.stab /usr/share/mlt/vid.stab
+rm libmlt-data_6.0.0-0ubuntu0~sunab~xenial1_all.deb
 
 # extract file from libvidstab1.0
 wget https://launchpad.net/~sunab/+archive/ubuntu/kdenlive-release/+files/libvidstab1.0_0.98b-0ubuntu0~sunab~xenial1_amd64.deb
 dpkg --fsys-tarfile libvidstab1.0_0.98b-0ubuntu0~sunab~xenial1_amd64.deb | sudo tar xf - -C /opt/vidstab
+rm libvidstab1.0_0.98b-0ubuntu0~sunab~xenial1_amd64.deb
 
 # install melt wrapper
 sudo wget --header='Accept-Encoding:none' -O /opt/vidstab/melt https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/video/melt-vidstab
