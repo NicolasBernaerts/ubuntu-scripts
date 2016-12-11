6dabf93745e16f8ef9b6b0169b5b646557f45581
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2016-12-11 10:11:07 +0100

    Update sweethome3d-install.sh

--- tools/sweethome3d-install.sh
+++ tools/sweethome3d-install.sh
@@ -11,3 +11,8 @@ mkdir $HOME/.local/apps
 wget -O sweethome3d.tgz https://vorboss.dl.sourceforge.net/project/sweethome3d/SweetHome3D/SweetHome3D-5.3/SweetHome3D-5.3-linux-x64.tgz
 tar -xvf sweethome3d.tgz
 mv SweetHome3D-* $HOME/.local/apps/SweetHome3D
+
+# declare desktop file
+wget -O $HOME/.local/share/applications/sweethome3d.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/tools/sweethome3d.desktop
+sed -i 's/##USER##/$USER/g' $HOME/.local/share/applications/sweethome3d.desktop
+chmod +x $HOME/.local/share/applications/sweethome3d.desktop
