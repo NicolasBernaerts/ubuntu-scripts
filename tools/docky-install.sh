4c05892b020fe66aeb8492898018e9c6f92021d3
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2016-12-28 15:16:08 +0100

    Create docky-install.sh

---
+++ tools/docky-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# -------------------------------------------------------
+# Script to install Sweet Home 3D
+# -------------------------------------------------------
+
+# sweethome3d
+sudo apt-get -y install docky
+
+# remove docky anchor
+gconftool-2 --type Boolean --set /apps/docky-2/Docky/Items/DockyItem/ShowDockyItem false
