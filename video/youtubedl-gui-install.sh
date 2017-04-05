4769d5104f8245713f7b809455f889dee52428ca
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2017-04-05 20:43:20 +0200

    Update youtubedl-gui-install.sh

--- video/youtubedl-gui-install.sh
+++ video/youtubedl-gui-install.sh
@@ -9,8 +9,9 @@ DISTRO=$(lsb_release -is 2>/dev/null)
 sudo apt-get update
 sudo apt-get -y install youtube-dl mkvtoolnix yad
 
-# install configuration file
-sudo wget --header='Accept-Encoding:none' -O $HOME/.config/youtubedl-gui.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/video/youtubedl-gui.conf
+# install configuration file and configure default directory
+wget --header='Accept-Encoding:none' -O $HOME/.config/youtubedl-gui.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/video/youtubedl-gui.conf
+sed -i "s|^directory=.*$|directory="$HOME"/Videos|" $HOME/.config/youtubedl-gui.conf
 
 # install main script
 sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/youtubedl-gui https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/video/youtubedl-gui
