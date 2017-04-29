183bd710d4f48bdd1dd693dca46eb5930d1c8a2e
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2017-04-29 12:42:10 +0200

    Update android-tools-install.sh

--- android/android-tools-install.sh
+++ android/android-tools-install.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 # Android Platform Tools installation
 
-# download zip from Google
+# install adb and fastboot
+sudo apt-get -y install android-tools-adb android-tools-fastboot
+
+# install latest version from Google
 wget --header='Accept-Encoding:none' https://dl.google.com/android/repository/platform-tools-latest-linux.zip
-
-# extract to /usr/local/sbin
 sudo unzip -d /usr/local/sbin platform-tools-latest-linux.zip
+rm platform-tools-latest-linux.zip
 
-# download adb launcher
+# launcher for latest adb
 sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/adb https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/android/adb
 sudo chmod +x /usr/local/sbin/adb
 
-# download fastboot launcher
+# launcher for latest fastboot
 sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/fastboot https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/android/fastboot
 sudo chmod +x /usr/local/sbin/fastboot
