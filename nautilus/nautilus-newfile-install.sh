f915c15f31ed9f7b23c0253cd8940fda2de133db
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2017-05-13 12:10:23 +0200

    Update nautilus-newfile-install.sh

--- nautilus/nautilus-newfile-install.sh
+++ nautilus/nautilus-newfile-install.sh
@@ -8,10 +8,6 @@ command -v nautilus >/dev/null 2>&1 || { echo "Nautilus has not been detected. P
 sudo apt-get update
 sudo apt-get -y install nautilus-actions
 
-# nautilus : remove nautilus action root menu
-mv $HOME/.config/nautilus-actions/nautilus-actions.conf $HOME/.config/nautilus-actions/nautilus-actions.conf.org
-cat $HOME/.config/nautilus-actions/nautilus-actions.conf.org | sed 's/items-create-root-menu=.*/items-create-root-menu=false/' > $HOME/.config/nautilus-actions/nautilus-actions.conf
-
 # install functions to read INI files
 sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/read-ini-file https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/tools/read-ini-file
 sudo chmod +x /usr/local/bin/read-ini-file
@@ -27,10 +23,10 @@ wget --header='Accept-Encoding:none' -O "$HOME/.config/nautilus-actions/nautilus
 
 # install empty docuemnt models
 mkdir --parents $HOME/.config/nautilus-actions/newfile
-wget --header='Accept-Encoding:none' -O "$HOME/.config/nautilus-actions/newfile/New Calc Sheet.ods" https://github.com/NicolasBernaerts/ubuntu-scripts/blob/master/nautilus/newfile/New%20Calc%20Sheet.ods?raw=true
-wget --header='Accept-Encoding:none' -O "$HOME/.config/nautilus-actions/newfile/New Writer Document.odt" https://github.com/NicolasBernaerts/ubuntu-scripts/blob/master/nautilus/newfile/New%20Writer%20Document.odt?raw=true
-wget --header='Accept-Encoding:none' -O "$HOME/.config/nautilus-actions/newfile/New Bash Script.sh" https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/nautilus/newfile/New%20Bash%20Script.sh
-wget --header='Accept-Encoding:none' -O "$HOME/.config/nautilus-actions/newfile/New File.txt" https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/nautilus/newfile/New%20File.txt
+wget --header='Accept-Encoding:none' -O "$HOME/.config/nautilus-actions/nautilus-newfile/Calc Sheet.ods" https://github.com/NicolasBernaerts/ubuntu-scripts/blob/master/nautilus/nautilus-newfile-models/Calc%20Sheet.ods?raw=true
+wget --header='Accept-Encoding:none' -O "$HOME/.config/nautilus-actions/nautilus-newfile/Writer Document.odt" https://github.com/NicolasBernaerts/ubuntu-scripts/blob/master/nautilus/nautilus-newfile-models/Writer%20Document.odt?raw=true
+wget --header='Accept-Encoding:none' -O "$HOME/.config/nautilus-actions/nautilus-newfile/Bash Script.sh" https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/nautilus/nautilus-newfile-models/Bash%20Script.sh
+wget --header='Accept-Encoding:none' -O "$HOME/.config/nautilus-actions/nautilus-newfile/Text File.txt" https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/nautilus/nautilus-newfile-models/Text%20File.txt
 
 # declare new empty files
 /usr/local/bin/nautilus-newfile-declare --install
