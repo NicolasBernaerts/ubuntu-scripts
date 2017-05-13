165b94cceba269c8da97791ec948c7595096dd0d
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2017-05-13 09:23:09 +0200

    Update pdf-rotate-install.sh

--- pdf/pdf-rotate-install.sh
+++ pdf/pdf-rotate-install.sh
@@ -7,8 +7,9 @@ command -v nautilus >/dev/null 2>&1 && sudo apt-get -y install nautilus-actions
 # main packages installation
 sudo apt-get -y install texlive-extra-utils
 
-# show icon in menus
+# show icon in menus (command different according to gnome version)
 gsettings set org.gnome.desktop.interface menus-have-icons true
+gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "{'Gtk/ButtonImages': <1>, 'Gtk/MenuImages': <1>}"
 
 # install icons
 sudo wget --header='Accept-Encoding:none' -O /usr/share/icons/pdf-menu.png https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/pdf/icons/pdf-menu.png
