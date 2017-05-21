214d97fa8dc75b0be4966af7ac0180343df874f9
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2017-05-21 23:38:06 +0200

    Create update-monitor-position-install.sh

---
+++ ubuntugnome/update-monitor-position-install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Install Update Monitor Position script
+
+# test Ubuntu or Linux Minty distribution
+DISTRO=$(lsb_release -is 2>/dev/null)
+[ "${DISTRO}" != "Ubuntu" ] && { zenity --error --text="This automatic installation script is for Ubuntu"; exit 1; }
+
+# install xmllint
+sudo apt-get -y install libxml2-utils
+
+# install script
+sudo wget -O /usr/local/sbin/update-monitor-position https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/ubuntugnome/update-monitor-position
+sudo chmod +x /usr/local/sbin/update-monitor-position
+
+# install desktop launcher
+sudo wget -O /usr/share/applications/update-monitor-position.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/ubuntugnome/update-monitor-position.desktop
+sudo chmod +x /usr/share/applications/update-monitor-position.desktop
+
+# declare autostart script
+mkdir -p $HOME/.config/autostart
+wget -O $HOME/.config/autostart/update-monitor-position.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/ubuntugnome/update-monitor-position.desktop
+sed -i -e 's/^Exec=.*$/Exec=update-monitor-position 5/' $HOME/.config/autostart/update-monitor-position.desktop
+chmod +x $HOME/.config/autostart/update-monitor-position.desktop
