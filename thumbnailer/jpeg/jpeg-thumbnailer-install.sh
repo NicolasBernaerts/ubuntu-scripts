431346ebdaa2d198f98dfef19b4a209c3efb7a96
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2017-10-03 00:35:52 +0200

    Create jpeg-thumbnailer-install.sh

---
+++ thumbnailer/jpeg/jpeg-thumbnailer-install.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+# Very fast JPEG thumbnailer with camera and location tag display
+
+# test Ubuntu distribution
+DISTRO=$(lsb_release -is 2>/dev/null)
+[ "${DISTRO}" != "Ubuntu" ] && { zenity --error --text="This automatic installation script is for Ubuntu only"; exit 1; }
+
+# install compilation tools
+sudo apt-get -y install build-essential cmake nasm git autoconf libtool
+
+# install libraries
+sudo apt install libjpeg-turbo8-dev libexif-dev
+
+# compile epeg
+mkdir ~/sources
+cd ~/sources
+git clone https://github.com/mattes/epeg.git
+cd epeg
+libtoolize
+autoreconf -i
+./configure
+make
+sudo make install
+
+# install thumbnailer icons
+sudo mkdir /usr/local/sbin/jpeg-thumbnailer.res
+sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/jpeg-thumbnailer.res/location.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/jpeg/location.png
+sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/jpeg-thumbnailer.res/none.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/jpeg/none.png
+
+# install main script
+sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/jpeg-thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/jpeg/jpeg-thumbnailer
+sudo chmod +rx /usr/local/sbin/jpeg-thumbnailer
+
+# thumbnailer integration
+sudo wget --header='Accept-Encoding:none' -O /usr/share/thumbnailers/jpeg.thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/jpeg/jpeg.thumbnailer
+
+# camera icons
+sudo mkdir --parents $HOME/.local/share/icons
+wget --header='Accept-Encoding:none' -O "$HOME/.local/share/icons/dmc-fz200.png" "https://raw.githubusercontent.com/NicolasBernaerts/icon/master/camera/dmc-fz200.png"
+wget --header='Accept-Encoding:none' -O "$HOME/.local/share/icons/dmc-tz5.png" "https://raw.githubusercontent.com/NicolasBernaerts/icon/master/camera/dmc-tz5.png"
+wget --header='Accept-Encoding:none' -O "$HOME/.local/share/icons/oneplus a2000.png" "https://raw.githubusercontent.com/NicolasBernaerts/icon/master/camera/oneplus a2000.png"
+wget --header='Accept-Encoding:none' -O "$HOME/.local/share/icons/oneplus a3003.png" "https://raw.githubusercontent.com/NicolasBernaerts/icon/master/camera/oneplus a3003.png"
+wget --header='Accept-Encoding:none' -O "$HOME/.local/share/icons/oneplus a5000.png" "https://raw.githubusercontent.com/NicolasBernaerts/icon/master/camera/oneplus a5000.png"
+
+# remove thumbnails
+rm -R $HOME/.cache/thumbnails/*
+
+# kill nautilus
+nautilus -q
