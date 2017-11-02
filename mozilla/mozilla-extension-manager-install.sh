9245d6fff1b9e07ae0e29cbfe85d443609453d50
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2017-11-02 10:27:17 +0100

    Update mozilla-extension-manager-install.sh

--- mozilla/mozilla-extension-manager-install.sh
+++ mozilla/mozilla-extension-manager-install.sh
@@ -12,11 +12,14 @@ sudo chmod +x /usr/local/bin/mozilla-extension-manager
 # AdBlockPlus
 mozilla-extension-manager --user --install https://addons.mozilla.org/firefox/downloads/latest/1865/addon-1865-latest.xpi
 
-# AppLauncher
-mozilla-extension-manager --user --install https://addons.mozilla.org/firefox/downloads/latest/61268/addon-61268-latest.xpi
-
 # FlashDisable
 mozilla-extension-manager --user --install https://addons.mozilla.org/firefox/downloads/latest/383235/addon-383235-latest.xpi
 
 # Print PDF
 mozilla-extension-manager --user --install https://addons.mozilla.org/firefox/downloads/latest/5971/platform:5/addon-5971-latest.xpi
+
+# Video DownloadHelper
+mozilla-extension-manager --user --install https://addons.mozilla.org/firefox/downloads/latest/video-downloadhelper/addon-3006-latest.xpi
+
+# Smart Proxy
+mozilla-extension-manager --user --install https://addons.mozilla.org/firefox/downloads/latest/smartproxy/platform:5/addon-845653-latest.xpi
