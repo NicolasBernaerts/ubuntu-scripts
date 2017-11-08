53a6d006b3b689c92d813aa6ef8536038564875b
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2017-11-08 19:29:27 +0100

    Update desktop-session.sh

--- desktop/desktop-session.sh
+++ desktop/desktop-session.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# Dump latest display environment and D-Bus address to /var/run/desktop-session.last
-#
-# This file is generated to be used by system daemon to send desktop notifications
+# Dump latest display environment variables to /tmp/.latest-desktop-session.ini
+# Variables are :
+#  - USER
+#  - DISPLAY
+#  - DBUS_SESSION_BUS_ADDRESS
 
 # desktop session data file
-DESKTOP_SESSION=/tmp/.desktop-session.ini
+DESKTOP_SESSION=/tmp/.latest-desktop-session.ini
 
 # Write current session data
 echo "[desktop-session]" > "${DESKTOP_SESSION}"
