496139c281d54294571509e3e3fe1317c0286152
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2017-11-26 10:13:09 +0100

    Update pdf-generate-install.sh

--- pdf/pdf-generate-install.sh
+++ pdf/pdf-generate-install.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
-# PDF generate extension
+# Install PDF generate extension
 
 # if nautilus present, install nautilus-actions
 command -v nautilus >/dev/null 2>&1 && sudo apt-get -y install nautilus-actions
 
+# main packages installation
+sudo apt-get -y install imagemagick unoconv ghostscript zenity libfile-mimeinfo-perl wkhtmltopdf
+
 # show icon in menus
 gsettings set org.gnome.desktop.interface menus-have-icons true
 
-# main packages installation
-sudo apt-get -y install imagemagick unoconv ghostscript zenity libfile-mimeinfo-perl wkhtmltopdf
+# install icons
+sudo wget --header='Accept-Encoding:none' -O /usr/share/icons/pdf-generate.png https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/pdf/icons/pdf-generate.png
 
 # install configuration file 
 mkdir --parents $HOME/.config
@@ -19,6 +22,5 @@ sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/pdf-generate https:/
 sudo chmod +x /usr/local/bin/pdf-generate
 
 # desktop integration
-#sudo wget --header='Accept-Encoding:none' -O /usr/share/applications/pdf-generate.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-generate.desktop
 mkdir --parents $HOME/.local/share/file-manager/actions
 wget --header='Accept-Encoding:none' -O $HOME/.local/share/file-manager/actions/pdf-generate-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-generate-action.desktop
