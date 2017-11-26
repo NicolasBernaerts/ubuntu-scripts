1a99428f6bedf555c1d140278a4e472a3277b794
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2017-11-26 10:19:07 +0100

    Update pdf-repair-install.sh

--- pdf/pdf-repair-install.sh
+++ pdf/pdf-repair-install.sh
@@ -11,8 +11,9 @@ sudo apt-get -y install gvfs-bin libnotify-bin ghostscript mupdf-tools gridsite-
 # if nautilus present, install nautilus-actions
 command -v nautilus >/dev/null 2>&1 && sudo apt-get -y install nautilus-actions
 
-# enable icon in nautilus menus
+# show icon in menus (command different according to gnome version)
 gsettings set org.gnome.desktop.interface menus-have-icons true
+gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "{'Gtk/ButtonImages': <1>, 'Gtk/MenuImages': <1>}"
 
 # install icons
 sudo wget --header='Accept-Encoding:none' -O /usr/share/icons/pdf-repair.png https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/pdf/icons/pdf-repair.png
