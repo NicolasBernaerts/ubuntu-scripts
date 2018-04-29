7a9e2edd0afb64df47ffe6e30bec3d246c04a416
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2018-04-29 11:45:54 +0200

    Update jpeg-thumbnailer-install.sh

--- thumbnailer/jpeg/jpeg-thumbnailer-install.sh
+++ thumbnailer/jpeg/jpeg-thumbnailer-install.sh
@@ -25,13 +25,16 @@ autoreconf -i
 make
 sudo make install
 
+# update local libraries
+sudo ldconfig
+
 # install thumbnailer icons
 sudo mkdir /usr/local/sbin/jpeg-thumbnailer.res
-sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/jpeg-thumbnailer.res/gps.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/jpeg/gps.png
-sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/jpeg-thumbnailer.res/none.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/jpeg/none.png
+sudo wget -O /usr/local/sbin/jpeg-thumbnailer.res/gps.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/jpeg/gps.png
+sudo wget -O /usr/local/sbin/jpeg-thumbnailer.res/none.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/jpeg/none.png
 
 # install main thumbnailer script
-sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/jpeg-thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/jpeg/jpeg-thumbnailer
+sudo wget -O /usr/local/sbin/jpeg-thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/jpeg/jpeg-thumbnailer
 sudo chmod +rx /usr/local/sbin/jpeg-thumbnailer
 
 # thumbnailer integration
@@ -39,16 +42,16 @@ sudo wget --header='Accept-Encoding:none' -O /usr/share/thumbnailers/jpeg.thumbn
 
 # camera icons
 sudo mkdir --parents $HOME/.local/share/icons
-wget --header='Accept-Encoding:none' -O "$HOME/.local/share/icons/canon eos 1000d.png" "https://raw.githubusercontent.com/NicolasBernaerts/icon/master/camera/canon eos 1000d.png"
-wget --header='Accept-Encoding:none' -O "$HOME/.local/share/icons/canon eos 1100d.png" "https://raw.githubusercontent.com/NicolasBernaerts/icon/master/camera/canon eos 1100d.png"
-wget --header='Accept-Encoding:none' -O "$HOME/.local/share/icons/canon powershot g7 x.png" "https://raw.githubusercontent.com/NicolasBernaerts/icon/master/camera/canon powershot g7 x.png"
-wget --header='Accept-Encoding:none' -O "$HOME/.local/share/icons/dmc-fz200.png" "https://raw.githubusercontent.com/NicolasBernaerts/icon/master/camera/dmc-fz200.png"
-wget --header='Accept-Encoding:none' -O "$HOME/.local/share/icons/dmc-tz5.png" "https://raw.githubusercontent.com/NicolasBernaerts/icon/master/camera/dmc-tz5.png"
-wget --header='Accept-Encoding:none' -O "$HOME/.local/share/icons/oneplus e1003.png" "https://raw.githubusercontent.com/NicolasBernaerts/icon/master/camera/oneplus e1003.png"
-wget --header='Accept-Encoding:none' -O "$HOME/.local/share/icons/oneplus a0001.png" "https://raw.githubusercontent.com/NicolasBernaerts/icon/master/camera/oneplus a0001.png"
-wget --header='Accept-Encoding:none' -O "$HOME/.local/share/icons/one a2003.png" "https://raw.githubusercontent.com/NicolasBernaerts/icon/master/camera/one a2003.png"
-wget --header='Accept-Encoding:none' -O "$HOME/.local/share/icons/oneplus a3003.png" "https://raw.githubusercontent.com/NicolasBernaerts/icon/master/camera/oneplus a3003.png"
-wget --header='Accept-Encoding:none' -O "$HOME/.local/share/icons/oneplus a5000.png" "https://raw.githubusercontent.com/NicolasBernaerts/icon/master/camera/oneplus a5000.png"
+wget -O "$HOME/.local/share/icons/canon eos 1000d.png" "https://raw.githubusercontent.com/NicolasBernaerts/icon/master/camera/canon eos 1000d.png"
+wget -O "$HOME/.local/share/icons/canon eos 1100d.png" "https://raw.githubusercontent.com/NicolasBernaerts/icon/master/camera/canon eos 1100d.png"
+wget -O "$HOME/.local/share/icons/canon powershot g7 x.png" "https://raw.githubusercontent.com/NicolasBernaerts/icon/master/camera/canon powershot g7 x.png"
+wget -O "$HOME/.local/share/icons/dmc-fz200.png" "https://raw.githubusercontent.com/NicolasBernaerts/icon/master/camera/dmc-fz200.png"
+wget -O "$HOME/.local/share/icons/dmc-tz5.png" "https://raw.githubusercontent.com/NicolasBernaerts/icon/master/camera/dmc-tz5.png"
+wget -O "$HOME/.local/share/icons/oneplus e1003.png" "https://raw.githubusercontent.com/NicolasBernaerts/icon/master/camera/oneplus e1003.png"
+wget -O "$HOME/.local/share/icons/oneplus a0001.png" "https://raw.githubusercontent.com/NicolasBernaerts/icon/master/camera/oneplus a0001.png"
+wget -O "$HOME/.local/share/icons/one a2003.png" "https://raw.githubusercontent.com/NicolasBernaerts/icon/master/camera/one a2003.png"
+wget -O "$HOME/.local/share/icons/oneplus a3003.png" "https://raw.githubusercontent.com/NicolasBernaerts/icon/master/camera/oneplus a3003.png"
+wget -O "$HOME/.local/share/icons/oneplus a5000.png" "https://raw.githubusercontent.com/NicolasBernaerts/icon/master/camera/oneplus a5000.png"
 
 # stop nautilus
 nautilus -q
