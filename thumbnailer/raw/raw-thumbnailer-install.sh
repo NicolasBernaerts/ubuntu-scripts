fa4a4e09bb240673fea14a83dbcb9e0494ff36d3
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2018-04-29 12:07:47 +0200

    Update raw-thumbnailer-install.sh

--- thumbnailer/raw/raw-thumbnailer-install.sh
+++ thumbnailer/raw/raw-thumbnailer-install.sh
@@ -5,15 +5,15 @@
 sudo apt-get -y install dcraw libjpeg-turbo-progs netpbm
 
 # install main script
-sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/raw-thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/raw/raw-thumbnailer
+sudo wget -O /usr/local/sbin/raw-thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/raw/raw-thumbnailer
 sudo chmod +x /usr/local/sbin/raw-thumbnailer
 
 # thumbnailer integration
-sudo wget --header='Accept-Encoding:none' -O /usr/share/thumbnailers/raw.thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/raw/raw.thumbnailer
+sudo wget -O /usr/share/thumbnailers/raw.thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/raw/raw.thumbnailer
 
 # stop nautilus
 nautilus -q
 
 # empty cache of previous thumbnails
-[ -d "$HOME/.cache/thumbnails" ] && rm -R $HOME/.cache/thumbnails/*
-[ -d "$HOME/.thumbnails" ] && rm -R $HOME/.thumbnails/*
+[ -d "$HOME/.cache/thumbnails" ] && rm --recursive --force $HOME/.cache/thumbnails/*
+[ -d "$HOME/.thumbnails" ] && rm --recursive --force $HOME/.thumbnails/*
