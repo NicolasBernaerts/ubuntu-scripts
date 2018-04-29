d02d88867499d2d8753bfe9285cee782de0420bf
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2018-04-29 02:15:28 +0200

    Update pdf-generate-install.sh

--- pdf/pdf-generate-install.sh
+++ pdf/pdf-generate-install.sh
@@ -5,24 +5,24 @@
 command -v nautilus >/dev/null 2>&1 && sudo apt-get -y install nautilus-actions
 
 # main packages installation
-sudo apt-get -y install imagemagick unoconv ghostscript zenity libfile-mimeinfo-perl wkhtmltopdf
+sudo apt -y install imagemagick unoconv ghostscript zenity libfile-mimeinfo-perl wkhtmltopdf
 
 # show icon in menus (command different according to gnome version)
 gsettings set org.gnome.desktop.interface menus-have-icons true
 gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "{'Gtk/ButtonImages': <1>, 'Gtk/MenuImages': <1>}"
 
 # install icons
-sudo wget --header='Accept-Encoding:none' -O /usr/share/icons/pdf-generate.png https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/pdf/icons/pdf-generate.png
+sudo wget -O /usr/share/icons/pdf-generate.png https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/pdf/icons/pdf-generate.png
 
 # install configuration file 
 mkdir --parents $HOME/.config
-wget --header='Accept-Encoding:none' -O $HOME/.config/pdf-generate.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-generate.conf
+wget -O $HOME/.config/pdf-generate.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-generate.conf
 
 # main script installation
-sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/pdf-generate https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-generate
+sudo wget -O /usr/local/bin/pdf-generate https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-generate
 sudo chmod +x /usr/local/bin/pdf-generate
 
 # desktop integration
-sudo wget --header='Accept-Encoding:none' -O /usr/share/applications/pdf-generate.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-generate.desktop
+sudo wget -O /usr/share/applications/pdf-generate.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-generate.desktop
 mkdir --parents $HOME/.local/share/file-manager/actions
-wget --header='Accept-Encoding:none' -O $HOME/.local/share/file-manager/actions/pdf-generate-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-generate-action.desktop
+wget -O $HOME/.local/share/file-manager/actions/pdf-generate-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-generate-action.desktop
