6687210aa4aa9f71bb007a230241fb6ac17c6256
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2018-04-29 12:06:05 +0200

    Update lo-thumbnailer-install.sh

--- thumbnailer/libreoffice/lo-thumbnailer-install.sh
+++ thumbnailer/libreoffice/lo-thumbnailer-install.sh
@@ -10,18 +10,18 @@ sudo apt-get -y install libfile-mimeinfo-perl gvfs-bin unzip netpbm
 
 # install thumbnailer icons
 sudo mkdir /usr/local/sbin/lo-thumbnailer.res
-sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/lo-thumbnailer.res/lo-database.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/icons/lo-database.png
-sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/lo-thumbnailer.res/lo-graphics.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/icons/lo-graphics.png
-sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/lo-thumbnailer.res/lo-presentation.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/icons/lo-presentation.png
-sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/lo-thumbnailer.res/lo-spreadsheet.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/icons/lo-spreadsheet.png
-sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/lo-thumbnailer.res/lo-text.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/icons/lo-text.png
+sudo wget -O /usr/local/sbin/lo-thumbnailer.res/lo-database.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/icons/lo-database.png
+sudo wget -O /usr/local/sbin/lo-thumbnailer.res/lo-graphics.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/icons/lo-graphics.png
+sudo wget -O /usr/local/sbin/lo-thumbnailer.res/lo-presentation.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/icons/lo-presentation.png
+sudo wget -O /usr/local/sbin/lo-thumbnailer.res/lo-spreadsheet.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/icons/lo-spreadsheet.png
+sudo wget -O /usr/local/sbin/lo-thumbnailer.res/lo-text.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/icons/lo-text.png
 
 # install main script
-sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/lo-thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/lo-thumbnailer
+sudo wget -O /usr/local/sbin/lo-thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/lo-thumbnailer
 sudo chmod +rx /usr/local/sbin/lo-thumbnailer
 
 # thumbnailer integration
-sudo wget --header='Accept-Encoding:none' -O /usr/share/thumbnailers/lo.thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/lo.thumbnailer
+sudo wget -O /usr/share/thumbnailers/lo.thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/lo.thumbnailer
 
 # if present, disable gsf-office.thumbnailer (used in UbuntuGnome 16.04)
 [ -f /usr/share/thumbnailers/gsf-office.thumbnailer ] && sudo mv /usr/share/thumbnailers/gsf-office.thumbnailer /usr/share/thumbnailers/gsf-office.thumbnailer.org
@@ -30,6 +30,6 @@ sudo wget --header='Accept-Encoding:none' -O /usr/share/thumbnailers/lo.thumbnai
 nautilus -q
 
 # remove previously cached files (thumbnails and masks)
-[ -d "$HOME/.cache/thumbnails" ] && rm -R $HOME/.cache/thumbnails/*
-[ -d "$HOME/.thumbnails" ] && rm -R $HOME/.thumbnails/*
-[ -d "$HOME/.cache/lo-thumbnailer" ] && rm -R $HOME/.cache/lo-thumbnailer
+[ -d "$HOME/.cache/thumbnails" ] && rm --recursive --force $HOME/.cache/thumbnails/*
+[ -d "$HOME/.thumbnails" ] && rm --recursive --force $HOME/.thumbnails/*
+[ -d "$HOME/.cache/lo-thumbnailer" ] && rm --recursive --force $HOME/.cache/lo-thumbnailer
