f813a9d75d856d1810ac817f9b254c00274717ed
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2018-04-29 12:07:06 +0200

    Update msoffice-thumbnailer-install.sh

--- thumbnailer/msoffice/msoffice-thumbnailer-install.sh
+++ thumbnailer/msoffice/msoffice-thumbnailer-install.sh
@@ -10,16 +10,16 @@ sudo apt-get -y install libfile-mimeinfo-perl gvfs-bin netpbm
 
 # install thumbnailer icons
 sudo mkdir /usr/local/sbin/msoffice-thumbnailer.res
-sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/msoffice-thumbnailer.res/msoffice-odt.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/msoffice/icons/msoffice-odt.png
-sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/msoffice-thumbnailer.res/msoffice-ods.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/msoffice/icons/msoffice-ods.png
-sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/msoffice-thumbnailer.res/msoffice-odp.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/msoffice/icons/msoffice-odp.png
+sudo wget -O /usr/local/sbin/msoffice-thumbnailer.res/msoffice-odt.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/msoffice/icons/msoffice-odt.png
+sudo wget -O /usr/local/sbin/msoffice-thumbnailer.res/msoffice-ods.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/msoffice/icons/msoffice-ods.png
+sudo wget -O /usr/local/sbin/msoffice-thumbnailer.res/msoffice-odp.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/msoffice/icons/msoffice-odp.png
 
 # install main script
-sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/msoffice-thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/msoffice/msoffice-thumbnailer
+sudo wget -O /usr/local/sbin/msoffice-thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/msoffice/msoffice-thumbnailer
 sudo chmod +rx /usr/local/sbin/msoffice-thumbnailer
 
 # thumbnailer integration
-sudo wget --header='Accept-Encoding:none' -O /usr/share/thumbnailers/msoffice.thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/msoffice/msoffice.thumbnailer
+sudo wget -O /usr/share/thumbnailers/msoffice.thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/msoffice/msoffice.thumbnailer
 
 # if present, disable gsf-office.thumbnailer (used in UbuntuGnome 16.04)
 [ -f /usr/share/thumbnailers/gsf-office.thumbnailer ] && sudo mv /usr/share/thumbnailers/gsf-office.thumbnailer /usr/share/thumbnailers/gsf-office.thumbnailer.org
@@ -28,6 +28,6 @@ sudo wget --header='Accept-Encoding:none' -O /usr/share/thumbnailers/msoffice.th
 nautilus -q
 
 # remove previously cached files (thumbnails and masks)
-[ -d "$HOME/.cache/thumbnails" ] && rm -R $HOME/.cache/thumbnails/*
-[ -d "$HOME/.thumbnails" ] && rm -R $HOME/.thumbnails/*
-[ -d "$HOME/.cache/msoffice-thumbnailer" ] && rm -R $HOME/.cache/msoffice-thumbnailer
+[ -d "$HOME/.cache/thumbnails" ] && rm --recursive --force $HOME/.cache/thumbnails/*
+[ -d "$HOME/.thumbnails" ] && rm --recursive --force $HOME/.thumbnails/*
+[ -d "$HOME/.cache/msoffice-thumbnailer" ] && rm --recursive --force $HOME/.cache/msoffice-thumbnailer
