d1ecee1baca0d2b1e6a998b0de6e495f00a0e763
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2018-06-03 10:48:25 +0200

    Update android-screenshot-install.sh

--- android/android-screenshot-install.sh
+++ android/android-screenshot-install.sh
@@ -5,11 +5,11 @@
 sudo apt-get -y install yad android-tools-adb
 
 # main script
-sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/android-screenshot https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/android/android-screenshot
+sudo wget -O /usr/local/bin/android-screenshot https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/android/android-screenshot
 sudo chmod +x /usr/local/bin/android-screenshot
 
 # configuration file
-wget --header='Accept-Encoding:none' -O $HOME/.android-screenshot.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/android/android-screenshot.conf
+wget -O $HOME/.android-screenshot.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/android/android-screenshot.conf
 
 # desktop integration
-sudo wget --header='Accept-Encoding:none' -O /usr/share/applications/android-screenshot.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/android/android-screenshot.desktop
+sudo wget -O /usr/share/applications/android-screenshot.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/android/android-screenshot.desktop
