c28ce1e302d1df26729191977eb5461a0619f057
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2018-08-07 18:29:26 +0200

    Update melt-vidstab-install.sh

--- video/melt-vidstab-install.sh
+++ video/melt-vidstab-install.sh
@@ -2,8 +2,8 @@
 # Install specific version of melt embedding vid.stab filter
 
 # test Ubuntu or Linux Minty distribution
-DISTRO=$(lsb_release -sc 2>/dev/null)
-[ "${DISTRO}" != "sarah" ] && [ "${DISTRO}" != "xenial" ] && { zenity --error --text="This automatic installation script is for Ubuntu Xenial or Linux Mint Sarah only"; exit 1; }
+DISTRO=$(lsb_release -is 2>/dev/null)
+[ "${DISTRO}" != "LinuxMint" ] && [ "${DISTRO}" != "Ubuntu" ] && { zenity --error --text="This automatic installation script is for Ubuntu or Linux Mint"; exit 1; }
 
 # create /opt/mlt-vidstab directory structure
 sudo mkdir --parent /opt/vidstab
