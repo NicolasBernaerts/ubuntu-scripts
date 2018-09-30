1da70ef470c54e521e7fdbdb3aa029ac840fef9f
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2018-09-30 23:28:39 +0200

    Update pdf-rotate-install.sh

--- pdf/pdf-rotate-install.sh
+++ pdf/pdf-rotate-install.sh
@@ -11,19 +11,19 @@ sudo apt-get -y install texlive-extra-utils
 gsettings set org.gnome.desktop.interface menus-have-icons true
 gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "{'Gtk/ButtonImages': <1>, 'Gtk/MenuImages': <1>}"
 
-# install icons
-sudo wget --header='Accept-Encoding:none' -O /usr/share/icons/pdf-menu.png https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/pdf/icons/pdf-menu.png
-sudo wget --header='Accept-Encoding:none' -O /usr/share/icons/rotate-left.png https://github.com/NicolasBernaerts/icon/raw/master/rotate-left.png
-sudo wget --header='Accept-Encoding:none' -O /usr/share/icons/rotate-right.png https://github.com/NicolasBernaerts/icon/raw/master/rotate-right.png
-sudo wget --header='Accept-Encoding:none' -O /usr/share/icons/revert-updown.png https://github.com/NicolasBernaerts/icon/raw/master/revert-updown.png
-
 # main script installation
-sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/pdf-rotate https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-rotate
+sudo wget -O /usr/local/bin/pdf-rotate https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-rotate
 sudo chmod +x /usr/local/bin/pdf-rotate
 
+# action icons
+wget -O $HOME/.local/share/icons/pdf-menu.png https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/pdf/icons/pdf-menu.png
+wget -O $HOME/.local/share/icons/rotate-left.png https://github.com/NicolasBernaerts/icon/raw/master/rotate-left.png
+wget -O $HOME/.local/share/icons/rotate-right.png https://github.com/NicolasBernaerts/icon/raw/master/rotate-right.png
+wget -O $HOME/.local/share/icons/revert-updown.png https://github.com/NicolasBernaerts/icon/raw/master/revert-updown.png
+
 # nautilus action integration
-sudo mkdir --parents /usr/share/file-manager/actions
-sudo wget --header='Accept-Encoding:none' -O /usr/share/file-manager/actions/pdf-rotate-menu.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-rotate-menu.desktop
-sudo wget --header='Accept-Encoding:none' -O /usr/share/file-manager/actions/pdf-rotate-left-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-rotate-left-action.desktop
-sudo wget --header='Accept-Encoding:none' -O /usr/share/file-manager/actions/pdf-rotate-right-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-rotate-right-action.desktop
-sudo wget --header='Accept-Encoding:none' -O /usr/share/file-manager/actions/pdf-rotate-updown-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-rotate-updown-action.desktop
+mkdir --parents $HOME/.local/share/file-manager/actions
+wget $HOME/.local/share/file-manager/actions/pdf-rotate-menu.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-rotate-menu.desktop
+wget $HOME/.local/share/file-manager/actions/pdf-rotate-left-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-rotate-left-action.desktop
+wget $HOME/.local/share/file-manager/actions/pdf-rotate-right-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-rotate-right-action.desktop
+wget $HOME/.local/share/file-manager/actions/pdf-rotate-updown-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-rotate-updown-action.desktop
