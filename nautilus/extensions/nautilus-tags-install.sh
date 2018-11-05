205077f30fa3e82b816c1641888e80747d1214a3
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2018-11-05 07:30:07 +0100

    Update nautilus-tags-install.sh

--- nautilus/extensions/nautilus-tags-install.sh
+++ nautilus/extensions/nautilus-tags-install.sh
@@ -10,9 +10,6 @@ sudo apt-get install python-nautilus python3-gi gir1.2-gexiv2-0.10 python-geopy
 # if needed, create extensions directory
 mkdir --parents "$HOME/.local/share/nautilus-python/extensions"
 
-# install python extension geopy.geocoders
-python -m pip install geopy
-
 # install nautilus extensions
 wget -O "$HOME/.local/share/nautilus-python/extensions/exif-alltags.py" https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/nautilus/extensions/exif-alltags.py
 wget -O "$HOME/.local/share/nautilus-python/extensions/exif-geotag.py" https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/nautilus/extensions/exif-geotag.py
