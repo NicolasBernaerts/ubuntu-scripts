78a51999f56c09292655fc667d8475f84874c56e
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2018-12-08 09:54:32 +0100

    Update lo-thumbnailer-install.sh

--- thumbnailer/libreoffice/lo-thumbnailer-install.sh
+++ thumbnailer/libreoffice/lo-thumbnailer-install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # LibreOffice thumbnailer
 
 # test Ubuntu distribution
@@ -8,13 +8,23 @@ DISTRO=$(lsb_release -is 2>/dev/null)
 # install tools
 sudo apt-get -y install libfile-mimeinfo-perl gvfs-bin unzip netpbm
 
+# create icons ressource directory
+ROOT_DOCTYPE="/usr/local/sbin/lo-thumbnailer.res"
+sudo mkdir "${ROOT_DOCTYPE}"
+
 # install thumbnailer icons
-sudo mkdir /usr/local/sbin/lo-thumbnailer.res
-sudo wget -O /usr/local/sbin/lo-thumbnailer.res/lo-database.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/icons/lo-database.png
-sudo wget -O /usr/local/sbin/lo-thumbnailer.res/lo-graphics.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/icons/lo-graphics.png
-sudo wget -O /usr/local/sbin/lo-thumbnailer.res/lo-presentation.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/icons/lo-presentation.png
-sudo wget -O /usr/local/sbin/lo-thumbnailer.res/lo-spreadsheet.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/icons/lo-spreadsheet.png
-sudo wget -O /usr/local/sbin/lo-thumbnailer.res/lo-text.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/icons/lo-text.png
+ARR_DOCTYPE=( "database" "graphics" "presentation" "spreadsheet" "text" )
+for DOCTYPE in "${ARR_DOCTYPE[@]}"
+do
+	# download document type icon
+	sudo wget -O "${ROOT_DOCTYPE}/lo-${DOCTYPE}.png" "https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/icons/lo-${DOCTYPE}.png"
+
+	# generate mask
+	sudo bash -c "pngtopnm ${ROOT_DOCTYPE}/lo-${DOCTYPE}.png | pnmscalefixed -xysize 256 256 - > ${ROOT_DOCTYPE}/lo-${DOCTYPE}.pnm" 
+
+	# generate alpha mask
+	sudo bash -c "pngtopnm -alpha ${ROOT_DOCTYPE}/lo-${DOCTYPE}.png | pnmscalefixed -xysize 256 256 - > ${ROOT_DOCTYPE}/lo-${DOCTYPE}-alpha.pnm" 
+done
 
 # install main script
 sudo wget -O /usr/local/sbin/lo-thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/lo-thumbnailer
@@ -26,20 +36,9 @@ sudo wget -O /usr/share/thumbnailers/lo.thumbnailer https://raw.githubuserconten
 # if present, disable gsf-office.thumbnailer (used in UbuntuGnome 16.04)
 [ -f "/usr/share/thumbnailers/gsf-office.thumbnailer" ] && sudo mv /usr/share/thumbnailers/gsf-office.thumbnailer /usr/share/thumbnailers/gsf-office.thumbnailer.org
 
-# if apparmor installed,
-if [ -d "/etc/apparmor.d" ]
-then
-  # declare the profile
-  sudo wget -O /etc/apparmor.d/usr.local.sbin.lo-thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/usr.local.sbin.lo-thumbnailer
-
-  # reload apparmor profiles
-  sudo service apparmor reload
-fi
-
 # stop nautilus
 nautilus -q
 
 # remove previously cached files (thumbnails and masks)
 [ -d "$HOME/.cache/thumbnails" ] && rm --recursive --force $HOME/.cache/thumbnails/*
 [ -d "$HOME/.thumbnails" ] && rm --recursive --force $HOME/.thumbnails/*
-[ -d "$HOME/.cache/lo-thumbnailer" ] && rm --recursive --force $HOME/.cache/lo-thumbnailer
