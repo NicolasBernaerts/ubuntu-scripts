a61e33de41c1d183682b875777dd5fc9a240a491
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2019-01-19 11:27:08 +0100

    Update gimp-tumbnailer-install.sh

--- thumbnailer/gimp/gimp-tumbnailer-install.sh
+++ thumbnailer/gimp/gimp-tumbnailer-install.sh
@@ -5,11 +5,22 @@
 DISTRO=$(lsb_release -is 2>/dev/null)
 [ "${DISTRO}" != "Ubuntu" ] && { zenity --error --text="This automatic installation script is for Ubuntu only"; exit 1; }
 
+# enable compilation chain
+#sudo apt -y install build-essential
+#sudo apt build-dep imagemagick
+#wget https://www.imagemagick.org/download/ImageMagick.tar.gz
+#tar xf ImageMagick.tar.gz
+#cd ImageMagick-7*
+#./configure --prefix=/usr/local
+#make
+#sudo make install
+#sudo ldconfig /usr/local/lib
+
 # install tools
-sudo apt-get -y install gvfs-bin xcftools netpbm
+sudo apt -y install xcftools netpbm
 
 # install bubblewrap wrapper to handle Nautilus 3.26.4+ bug for external thumbnailers
-sudo wget -O /usr/local/bin/bwrap https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/nautilus/bwrap
+[ ! -f /usr/local/bin/bwrap ] && sudo wget -O /usr/local/bin/bwrap https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/nautilus/bwrap
 sudo chmod +rx /usr/local/bin/bwrap
 
 # install main script
