1d18be1ed7851cd4ad081bbfe1155e1465cc8970
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2019-11-13 02:43:25 +0100

    Update sshfs-mount-install.sh

--- sshfs/sshfs-mount-install.sh
+++ sshfs/sshfs-mount-install.sh
@@ -14,18 +14,13 @@ CONFIG_INDEX="$2"
 NAME=$(grep "^name=" "${CONFIG_FILE}" | cut -d'=' -f2)
 LABEL=$(grep "^label=" "${CONFIG_FILE}" | cut -d'=' -f2)
 ICON=$(grep "^icon=" "${CONFIG_FILE}" | cut -d'=' -f2)
-SERVER_ACCOUNT=$(grep "^account=" "${CONFIG_FILE}" | cut -d'=' -f2)
-SERVER_LABEL=$(grep "^${CONFIG_INDEX}-label=" "${CONFIG_FILE}" | cut -d'=' -f2)
-SERVER_ADDRESS=$(grep "^${CONFIG_INDEX}-address=" "${CONFIG_FILE}" | cut -d'=' -f2)
-SERVER_PORT=$(grep "^${CONFIG_INDEX}-port=" "${CONFIG_FILE}" | cut -d'=' -f2)
+ACCOUNT=$(grep "^account=" "${CONFIG_FILE}" | cut -d'=' -f2)
 
 # check configuration data
-[ "${ICON}" = "" ] && ICON="server"
-[ "${SERVER_PORT}" = "" ] && SERVER_PORT=22
 [ "${NAME}" = "" ] && { echo "Ressource name not defined in ${CONFIG_FILE}"; exit 1; }
 [ "${LABEL}" = "" ] && { echo "Ressource label not defined in ${CONFIG_FILE}"; exit 1; }
-[ "${SERVER_ACCOUNT}" = "" ] && { echo "Server account not defined in ${CONFIG_FILE}"; exit 1; }
-[ "${SERVER_ADDRESS}" = "" ] && { echo "Server URL not defined for index ${CONFIG_INDEX}"; exit 1; }
+[ "${ACCOUNT}" = "" ] && { echo "Server account not defined in ${CONFIG_FILE}"; exit 1; }
+[ "${ICON}" = "" ] && ICON="server"
 
 # install sshfs
 echo "install packages"
@@ -49,22 +44,57 @@ wget https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/ss
 cat ./sshfs-mount.desktop | sed "s|NAME|${NAME}|g" | sed "s|LABEL|${LABEL}|g" | sed "s|ICON|${ICON}|g" > "${SSHFS_LAUNCHER}"
 chmod +x "${SSHFS_LAUNCHER}"
 
-# setup SSH config for SSHFS auto-mmount
-echo "set SSH config"
-INTERVAL=$(grep "ServerAliveInterval" "$HOME/.ssh/config")
-[ "${INTERVAL}" = "" ] && echo "ServerAliveInterval 15" >> "$HOME/.ssh/config"
-AUTHKEY=$(grep "PubkeyAuthentication" "$HOME/.ssh/config")
-[ "${AUTHKEY}" = "" ] && echo "PubkeyAuthentication yes" >> "$HOME/.ssh/config"
-chmod 600 "$HOME/.ssh/config"
-
 # if needed, generate keys
 echo "generate SSH keys"
 [ ! -f "$HOME/.ssh/id_rsa" ] && ssh-keygen -t rsa
 
-# upload public key to SSH server
-echo "upload public key to server"
-ssh-copy-id -p "${SERVER_PORT}" "${SERVER_ACCOUNT}@${SERVER_ADDRESS}"
+# loop to declare hosts in SSH config
+echo "set SSH config"
+SSH_CONFIG="$HOME/.ssh/config"
+INDEX=1
+while [ ${INDEX} -gt 0 ]
+do
+	# read configuration
+	SERVER_ADDRESS=$(grep "^${INDEX}-address=" "${CONFIG_FILE}" | cut -d'=' -f2)
+	SERVER_PORT=$(grep "^${INDEX}-port=" "${CONFIG_FILE}" | cut -d'=' -f2)
+	[ "${SERVER_ADDRESS}" = "" ] && INDEX=0
+	[ "${SERVER_PORT}" = "" ] && SERVER_PORT=22
 
-# test the connexion
-echo "first connexion on ${SERVER_LABEL}"
-ssh -v -p "${SERVER_PORT}" "${SERVER_ACCOUNT}@${SERVER_ADDRESS}"
+	# if server configuration is read
+	if [ ${INDEX} -gt 0 ]
+	then
+		# display progress
+		echo " - server ${SERVER_ADDRESS}"
+
+		# add host to file
+		echo " " >> "${SSH_CONFIG}"
+		echo "host ${NAME}_${INDEX}" >> "${SSH_CONFIG}"
+		echo "HostName ${SERVER_ADDRESS}" >> "${SSH_CONFIG}"
+		echo "Port ${SERVER_PORT}" >> "${SSH_CONFIG}"
+		echo "User ${ACCOUNT}" >> "${SSH_CONFIG}"
+		echo "ServerAliveInterval 15" >> "${SSH_CONFIG}"
+		echo "PubkeyAuthentication yes" >> "${SSH_CONFIG}"
+		echo "StrictHostKeyChecking no" >> "${SSH_CONFIG}"
+		echo "Compression yes" >> "${SSH_CONFIG}"
+		echo "CompressionLevel 9" >> "${SSH_CONFIG}"
+		echo " " >> "${SSH_CONFIG}"
+
+		# set access mode to 600
+		chmod 600 "${SSH_CONFIG}"
+
+		# if index is the one given as parameters
+		if [ ${INDEX} -eq ${CONFIG_INDEX} ]
+		then
+			# upload public key to SSH server
+			echo "   * upload public key to server"
+			ssh-copy-id -p "${SERVER_PORT}" "${ACCOUNT}@${SERVER_ADDRESS}"
+
+			# test the connexion
+			echo "   * first connexion"
+			ssh -p "${SERVER_PORT}" "${ACCOUNT}@${SERVER_ADDRESS}"
+		fi
+
+		# increment index
+		INDEX=$((INDEX + 1))
+	fi
+done
