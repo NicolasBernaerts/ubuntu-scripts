a49192a4d862357349c10d0d3a3c48ede6a1f7d1
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2020-05-01 19:15:00 +0200

    Update and rename generate-booklet-install.sh to pdf-booklet-install.sh

--- pdf/pdf-booklet-install.sh
+++ pdf/pdf-booklet-install.sh
@@ -9,17 +9,17 @@ DISTRO=$(lsb_release -is 2>/dev/null)
 sudo apt-get -y install poppler-utils texlive-extra-utils mimetype unoconv
 
 # if nautilus present, install nautilus-actions
-command -v nautilus >/dev/null 2>&1 && sudo apt-get -y install nautilus-actions
+#command -v nautilus >/dev/null 2>&1 && sudo apt-get -y install nautilus-actions
 
 # show icon in menus (command different according to gnome version)
 gsettings set org.gnome.desktop.interface menus-have-icons true
 gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "{'Gtk/ButtonImages': <1>, 'Gtk/MenuImages': <1>}"
 
 # install main script
-sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/generate-booklet https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/generate-booklet
-sudo chmod +x /usr/local/bin/generate-booklet
+sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/pdf-booklet https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-booklet
+sudo chmod +x /usr/local/bin/pdf-booklet
 
 # desktop integration
-sudo wget --header='Accept-Encoding:none' -O /usr/share/applications/generate-booklet.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/generate-booklet.desktop
+sudo wget --header='Accept-Encoding:none' -O /usr/share/applications/pdf-booklet.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-booklet.desktop
 mkdir --parents $HOME/.local/share/file-manager/actions
-wget --header='Accept-Encoding:none' -O $HOME/.local/share/file-manager/actions/generate-booklet-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/generate-booklet-action.desktop
+wget --header='Accept-Encoding:none' -O $HOME/.local/share/file-manager/actions/pdf-booklet-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-booklet-action.desktop
