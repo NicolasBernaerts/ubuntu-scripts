acafd3ca20b5ca86c449f02398a6969295adf368
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2020-05-02 19:17:09 +0200

    Update pdf-repair-install.sh

--- pdf/pdf-repair-install.sh
+++ pdf/pdf-repair-install.sh
@@ -6,7 +6,7 @@ DISTRO=$(lsb_release -is 2>/dev/null)
 [ "${DISTRO}" != "Ubuntu" ] && { zenity --error --text="This automatic installation script is for Ubuntu only"; exit 1; }
 
 # install tools
-sudo apt-get -y install gvfs-bin libnotify-bin ghostscript mupdf-tools gridsite-clients
+sudo apt-get -y install ghostscript mupdf-tools
 
 # if nautilus present, install nautilus-actions
 command -v nautilus >/dev/null 2>&1 && sudo apt-get -y install nautilus-actions
