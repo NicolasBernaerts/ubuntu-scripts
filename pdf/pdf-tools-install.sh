248e73030630eb7e0c06bd2976dffdf5e6e9346c
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2020-05-02 23:57:27 +0200

    Update pdf-tools-install.sh

--- pdf/pdf-tools-install.sh
+++ pdf/pdf-tools-install.sh
@@ -15,6 +15,10 @@ then
   rm ./imagemagisk-enable-pdf-install.sh
 fi
 
+# main packages installation
+sudo apt -y install texlive-extra-utils
+sudo apt -y install ghostscript mupdf-tools
+
 # if nautilus present, install nautilus-actions
 command -v nautilus >/dev/null 2>&1 && sudo apt-get -y install nautilus-actions
 
@@ -22,14 +26,44 @@ command -v nautilus >/dev/null 2>&1 && sudo apt-get -y install nautilus-actions
 gsettings set org.gnome.desktop.interface menus-have-icons true
 gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "{'Gtk/ButtonImages': <1>, 'Gtk/MenuImages': <1>}"
 
-# install icons
+# install main menu icon
 sudo wget -O /usr/share/icons/pdf-menu.png https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/pdf/icons/pdf-menu.png
+
+# if needed, create nautilus python extensions folder
+mkdir --parents $HOME/.local/share/nautilus-python/extensions
+wget -O $HOME/.local/share/nautilus-python/extensions/pdf-tools-menu.py https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-booklet-menu.py
+
+# -----------------
+#  PDF compression
+# -----------------
+
+# install icon
 sudo wget -O /usr/share/icons/pdf-compress.png https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/pdf/icons/pdf-compress.png
 
 # install main script
 sudo wget -O /usr/local/bin/pdf-compress https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-compress
 sudo chmod +x /usr/local/bin/pdf-compress
 
-# desktop integration
-mkdir --parents $HOME/.local/share/file-manager/actions
-wget -O $HOME/.local/share/file-manager/actions/pdf-compress-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-compress-action.desktop
+# -----------------
+#  PDF repair
+# -----------------
+
+# install icon
+sudo wget -O /usr/share/icons/pdf-repair.png https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/pdf/icons/pdf-repair.png
+
+# install main script
+sudo wget -O /usr/local/bin/pdf-repair https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-repair
+sudo chmod +x /usr/local/bin/pdf-repair
+
+# -----------------
+#  PDF rotate
+# -----------------
+
+# install icons
+sudo wget -O /usr/share/icons/rotate-left.png https://github.com/NicolasBernaerts/icon/raw/master/rotate-left.png
+sudo wget -O /usr/share/icons/rotate-right.png https://github.com/NicolasBernaerts/icon/raw/master/rotate-right.png
+sudo wget -O /usr/share/icons/rotate-updown.png https://github.com/NicolasBernaerts/icon/raw/master/revert-updown.png
+
+# install main script
+sudo wget -O /usr/local/bin/pdf-rotate https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-rotate
+sudo chmod +x /usr/local/bin/pdf-rotate
