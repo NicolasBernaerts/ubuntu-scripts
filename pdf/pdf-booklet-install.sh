70e034863e7a9e33caa3e91f291c4c6bf92d5257
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2020-05-02 17:14:29 +0200

    Update pdf-booklet-install.sh

--- pdf/pdf-booklet-install.sh
+++ pdf/pdf-booklet-install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Install PDF booklet generation extension
+# Uses Nautilus and Python3 wrapper
 
 # test Ubuntu distribution
 DISTRO=$(lsb_release -is 2>/dev/null)
@@ -8,8 +9,8 @@ DISTRO=$(lsb_release -is 2>/dev/null)
 # install tools
 sudo apt-get -y install poppler-utils texlive-extra-utils unoconv
 
-# if nautilus present, install nautilus-actions
-#command -v nautilus >/dev/null 2>&1 && sudo apt-get -y install nautilus-actions
+# if nautilus present, install nautilus python3 wrapper
+command -v nautilus >/dev/null 2>&1 && sudo apt -y install python3-nautilus
 
 # show icon in menus (command different according to gnome version)
 gsettings set org.gnome.desktop.interface menus-have-icons true
@@ -19,10 +20,10 @@ gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "{'Gtk/Butto
 sudo wget -O /usr/share/icons/pdf-booklet.png https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/pdf/icons/pdf-booklet.png
 
 # install main script
-sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/pdf-booklet https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-booklet
+sudo wget -O /usr/local/bin/pdf-booklet https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-booklet
 sudo chmod +x /usr/local/bin/pdf-booklet
 
 # desktop integration
-sudo wget --header='Accept-Encoding:none' -O /usr/share/applications/pdf-booklet.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-booklet.desktop
-mkdir --parents $HOME/.local/share/file-manager/actions
-wget --header='Accept-Encoding:none' -O $HOME/.local/share/file-manager/actions/pdf-booklet-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-booklet-action.desktop
+sudo wget -O /usr/share/applications/pdf-booklet.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-booklet.desktop
+mkdir --parents $HOME/.local/share/nautilus-python/extensions
+wget --header='Accept-Encoding:none' -O $HOME/.local/share/nautilus-python/extensions/pdf-booklet-menu.py https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-booklet-menu.py
