cb861e36f7bb108c633ea228cf00e8d7d2a32348
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2020-05-03 00:11:33 +0200

    Update pdf-tools-menu.py

--- pdf/pdf-tools-menu.py
+++ pdf/pdf-tools-menu.py
@@ -39,7 +39,7 @@ class PDFToolsMenuProvider(GObject.GObject, Nautilus.MenuProvider):
       # create sub-menu item PDF Compress
       file = pathlib.Path("/usr/local/bin/pdf-compress")
       if file.exists ():
-        pdf_compress = Nautilus.MenuItem(name='PDFToolsMenuProvider::pdf_compress', label='Compress scanned PDF', icon='/usr/share/icons/pdf-compress.png')
+        pdf_compress = Nautilus.MenuItem(name='PDFToolsMenuProvider::pdf_compress', label='Compress scanned document', icon='/usr/share/icons/pdf-compress.png')
         pdf_compress.connect('activate', self.pdf_compress, filename)
         submenu.append_item(pdf_compress)
 
@@ -47,12 +47,12 @@ class PDFToolsMenuProvider(GObject.GObject, Nautilus.MenuProvider):
       file = pathlib.Path("/usr/local/bin/pdf-repair")
       if file.exists ():
         # ghostscript method
-        pdf_repair_ghostscript = Nautilus.MenuItem(name='PDFToolsMenuProvider::pdf_repair_ghostscript', label='Repair PDF (Ghostscript)', icon='/usr/share/icons/pdf-repair.png')
+        pdf_repair_ghostscript = Nautilus.MenuItem(name='PDFToolsMenuProvider::pdf_repair_ghostscript', label='Repair (Ghostscript)', icon='/usr/share/icons/pdf-repair.png')
         pdf_repair_ghostscript.connect('activate', self.pdf_repair_ghostscript, filename)
         submenu.append_item(pdf_repair_ghostscript)
 
         # mutool method
-        pdf_repair_mutool = Nautilus.MenuItem(name='PDFToolsMenuProvider::pdf_repair_mutool', label='Repair PDF (MU Tool)', icon='/usr/share/icons/pdf-repair.png')
+        pdf_repair_mutool = Nautilus.MenuItem(name='PDFToolsMenuProvider::pdf_repair_mutool', label='Repair (MU Tool)', icon='/usr/share/icons/pdf-repair.png')
         pdf_repair_mutool.connect('activate', self.pdf_repair_mutool, filename)
         submenu.append_item(pdf_repair_mutool)
 
@@ -60,17 +60,17 @@ class PDFToolsMenuProvider(GObject.GObject, Nautilus.MenuProvider):
       file = pathlib.Path("/usr/local/bin/pdf-rotate")
       if file.exists ():
         # rotate right
-        pdf_rotate_right = Nautilus.MenuItem(name='PDFToolsMenuProvider::pdf_rotate_right', label='Rotate PDF Right (+90°)', icon='/usr/share/icons/rotate-right.png')
+        pdf_rotate_right = Nautilus.MenuItem(name='PDFToolsMenuProvider::pdf_rotate_right', label='Rotate Right (+90°)', icon='/usr/share/icons/rotate-right.png')
         pdf_rotate_right.connect('activate', self.pdf_rotate_right, filename)
         submenu.append_item(pdf_rotate_right)
 
         # rotate left
-        pdf_rotate_left = Nautilus.MenuItem(name='PDFToolsMenuProvider::pdf_rotate_left', label='Rotate PDF Left (-90°)', icon='/usr/share/icons/rotate-left.png')
+        pdf_rotate_left = Nautilus.MenuItem(name='PDFToolsMenuProvider::pdf_rotate_left', label='Rotate Left (-90°)', icon='/usr/share/icons/rotate-left.png')
         pdf_rotate_left.connect('activate', self.pdf_rotate_left, filename)
         submenu.append_item(pdf_rotate_left)
 
         # rotate 180
-        pdf_rotate_updown = Nautilus.MenuItem(name='PDFToolsMenuProvider::pdf_rotate_updown', label='Rotate PDF Upside Down (+180°)', icon='/usr/share/icons/rotate-updown.png')
+        pdf_rotate_updown = Nautilus.MenuItem(name='PDFToolsMenuProvider::pdf_rotate_updown', label='Rotate Upside Down (+180°)', icon='/usr/share/icons/rotate-updown.png')
         pdf_rotate_updown.connect('activate', self.pdf_rotate_updown, filename)
         submenu.append_item(pdf_rotate_updown)
 
