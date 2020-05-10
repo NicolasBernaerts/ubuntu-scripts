19690d3b97a3060701b0d0ecc7e4ebfd42ab658b
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2020-05-10 23:04:42 +0200

    Update scan2folder-install.sh

--- scanner/scan2folder-install.sh
+++ scanner/scan2folder-install.sh
@@ -2,7 +2,7 @@
 # scan2folder tool
 
 # install tools
-sudo apt-get -y install ghostscript zenity
+sudo apt-get -y install ghostscript yad hplip-gui
 
 # install yad
 IS_PRESENT=$(command -v yad)
@@ -14,13 +14,13 @@ then
 fi
 
 # main script
-sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/scan2folder https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/scanner/scan2folder
+sudo wget -O /usr/local/bin/scan2folder https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/scanner/scan2folder
 sudo chmod +x /usr/local/bin/scan2folder
 
 # configuration file
 mkdir --parent $HOME/.config
-wget --header='Accept-Encoding:none' -O $HOME/.config/scan2folder.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/scanner/scan2folder.conf
+wget -O $HOME/.config/scan2folder.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/scanner/scan2folder.conf
 
 # desktop integration
-sudo wget --header='Accept-Encoding:none' -O /usr/share/applications/scan2folder.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/scanner/scan2folder.desktop
+sudo wget -O /usr/share/applications/scan2folder.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/scanner/scan2folder.desktop
 sudo chmod +x /usr/share/applications/scan2folder.desktop
