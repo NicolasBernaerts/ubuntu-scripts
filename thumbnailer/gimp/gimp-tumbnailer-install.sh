cc1e2aaf628baf67026d22ff78ea79348450f67d
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2020-05-10 22:29:32 +0200

    Update gimp-tumbnailer-install.sh

--- thumbnailer/gimp/gimp-tumbnailer-install.sh
+++ thumbnailer/gimp/gimp-tumbnailer-install.sh
@@ -5,17 +5,6 @@
 DISTRO=$(lsb_release -is 2>/dev/null)
 [ "${DISTRO}" != "Ubuntu" ] && { zenity --error --text="This automatic installation script is for Ubuntu only"; exit 1; }
 
-# enable compilation chain
-#sudo apt -y install build-essential
-#sudo apt build-dep imagemagick
-#wget https://www.imagemagick.org/download/ImageMagick.tar.gz
-#tar xf ImageMagick.tar.gz
-#cd ImageMagick-7*
-#./configure --prefix=/usr/local
-#make
-#sudo make install
-#sudo ldconfig /usr/local/lib
-
 # install tools
 sudo apt -y install xcftools netpbm
 
@@ -24,11 +13,11 @@ sudo apt -y install xcftools netpbm
 sudo chmod +rx /usr/local/bin/bwrap
 
 # install main script
-sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/gimp-thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/gimp/gimp-thumbnailer
+sudo wget -O /usr/local/sbin/gimp-thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/gimp/gimp-thumbnailer
 sudo chmod +x /usr/local/sbin/gimp-thumbnailer
 
 # thumbnailer integration
-sudo wget --header='Accept-Encoding:none' -O /usr/share/thumbnailers/gimp.thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/gimp/gimp.thumbnailer
+sudo wget -O /usr/share/thumbnailers/gimp.thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/gimp/gimp.thumbnailer
 
 # stop nautilus
 nautilus -q
