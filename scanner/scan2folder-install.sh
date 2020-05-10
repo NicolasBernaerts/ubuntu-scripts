589555b0556b266249d2692a9ce579d6ee111055
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2020-05-10 23:05:33 +0200

    Update scan2folder-install.sh

--- scanner/scan2folder-install.sh
+++ scanner/scan2folder-install.sh
@@ -4,15 +4,6 @@
 # install tools
 sudo apt -y install ghostscript yad hplip-gui
 
-# install yad
-IS_PRESENT=$(command -v yad)
-if [ -z "${IS_PRESENT}" ]
-then
-  sudo add-apt-repository -y ppa:webupd8team/y-ppa-manager
-  sudo apt-get update
-  sudo apt-get -y install yad
-fi
-
 # main script
 sudo wget -O /usr/local/bin/scan2folder https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/scanner/scan2folder
 sudo chmod +x /usr/local/bin/scan2folder
