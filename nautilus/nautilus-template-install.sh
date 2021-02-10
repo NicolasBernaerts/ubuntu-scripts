cffb55bd9f1a2bc084b34cad2afe30ac3bf2f790
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2021-02-10 16:13:55 +0100

    Update nautilus-template-install.sh

--- nautilus/nautilus-template-install.sh
+++ nautilus/nautilus-template-install.sh
@@ -9,10 +9,10 @@ DIR_TEMPLATE=$(xdg-user-dir TEMPLATES)
 if [ -d "${DIR_TEMPLATE}" ]
 then
   # download templates
-  wget -O "${DIR_TEMPLATE}/Calc Sheet.ods" https://github.com/NicolasBernaerts/ubuntu-scripts/blob/master/nautilus/nautilus-newfile-models/Calc%20Sheet.ods?raw=true
-  wget -O "${DIR_TEMPLATE}/Writer Document.odt" https://github.com/NicolasBernaerts/ubuntu-scripts/blob/master/nautilus/nautilus-newfile-models/Writer%20Document.odt?raw=true
-  wget -O "${DIR_TEMPLATE}/Bash Script.sh" https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/nautilus/nautilus-newfile-models/Bash%20Script.sh
-  wget -O "${DIR_TEMPLATE}/Text File.txt" https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/nautilus/nautilus-newfile-models/Text%20File.txt
+  wget -O "${DIR_TEMPLATE}/Calc Sheet.ods" https://github.com/NicolasBernaerts/ubuntu-scripts/blob/master/nautilus/nautilus-template/Calc%20Sheet.ods?raw=true
+  wget -O "${DIR_TEMPLATE}/Writer Document.odt" https://github.com/NicolasBernaerts/ubuntu-scripts/blob/master/nautilus/nautilus-template/Writer%20Document.odt?raw=true
+  wget -O "${DIR_TEMPLATE}/Bash Script.sh" https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/nautilus/nautilus-template/Bash%20Script.sh
+  wget -O "${DIR_TEMPLATE}/Text File.txt" https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/nautilus/nautilus-template/Text%20File.txt
 
   # set execute attributes
   chmod +x "${DIR_TEMPLATE}/Bash Script.sh"
