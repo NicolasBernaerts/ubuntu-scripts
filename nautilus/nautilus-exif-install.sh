c6892ca77c12e6080555c52da3a9dbfde1ea286c
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2024-12-07 08:35:01 +0100

    Update nautilus-exif-install.sh

--- nautilus/nautilus-exif-install.sh
+++ nautilus/nautilus-exif-install.sh
@@ -14,7 +14,7 @@ mkdir --parents "$HOME/.local/share/nautilus-python/extensions"
 # install nautilus extensions
 wget -O "$HOME/.local/share/nautilus-python/extensions/exif-columns.py" https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/nautilus/extensions/exif-columns.py
 wget -O "$HOME/.local/share/nautilus-python/extensions/exif-tags.py" https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/nautilus/extensions/exif-tags.py
-wget -O "$HOME/.local/share/nautilus-python/extensions/exif-geotag.py" https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/nautilus/extensions/exif-geotag.py
+#wget -O "$HOME/.local/share/nautilus-python/extensions/exif-geotag.py" https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/nautilus/extensions/exif-geotag.py
 
 # close nautilus to force extension reload
 nautilus -q
