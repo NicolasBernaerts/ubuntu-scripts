6b6bddc938413fb157ba957167adf349548e33e1
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2025-04-30 22:48:30 +0200

    Update pdf-rotate-install.sh

--- pdf/pdf-rotate-install.sh
+++ pdf/pdf-rotate-install.sh
@@ -16,16 +16,16 @@ sudo apt -y install texlive-extra-utils
 gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "{'Gtk/ButtonImages': <1>, 'Gtk/MenuImages': <1>}"
 
 # install icons
-sudo wget -O /usr/share/icons/pdf-rotate.png https://github.com/NicolasBernaerts/icon/raw/master/pdf/pdf-rotate.png
-sudo wget -O /usr/share/icons/rotate-left.png https://github.com/NicolasBernaerts/icon/raw/master/rotation/rotate-left.png
-sudo wget -O /usr/share/icons/rotate-right.png https://github.com/NicolasBernaerts/icon/raw/master/rotation/rotate-right.png
-sudo wget -O /usr/share/icons/rotate-updown.png https://github.com/NicolasBernaerts/icon/raw/master/rotation/rotate-updown.png
+sudo wget -O /usr/share/icons/pdf-rotate.png https://github.com/NicolasBernaerts/icon/blob/master/pdf/pdf-rotate.png
+sudo wget -O /usr/share/icons/rotate-left.png https://github.com/NicolasBernaerts/icon/blob/master/rotation/rotate-left.png
+sudo wget -O /usr/share/icons/rotate-right.png https://github.com/NicolasBernaerts/icon/blob/master/rotation/rotate-right.png
+sudo wget -O /usr/share/icons/rotate-updown.png https://github.com/NicolasBernaerts/icon/blob/master/rotation/rotate-updown.png
 
 # main script installation
-sudo wget -O /usr/local/bin/pdf-rotate https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-rotate
+sudo wget -O /usr/local/bin/pdf-rotate https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/pdf/pdf-rotate
 sudo chmod +x /usr/local/bin/pdf-rotate
 
 # desktop integration
 mkdir --parents $HOME/.local/share/nautilus-python/extensions
-wget -O $HOME/.local/share/nautilus-python/extensions/pdf-rotate-menu.py https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-rotate-menu.py
+wget -O $HOME/.local/share/nautilus-python/extensions/pdf-rotate-menu.py https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/pdf/pdf-rotate-menu.py
 
