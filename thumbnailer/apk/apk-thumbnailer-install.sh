44fb87a355d2ce9d97a9b1b2b2aaeee4449f9bbd
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2025-04-30 23:24:23 +0200

    Update apk-thumbnailer-install.sh

--- thumbnailer/apk/apk-thumbnailer-install.sh
+++ thumbnailer/apk/apk-thumbnailer-install.sh
@@ -9,15 +9,15 @@ DISTRO=$(lsb_release -is 2>/dev/null)
 sudo apt-get -y install gvfs-bin aapt unzip netpbm
 
 # install bubblewrap wrapper to handle Nautilus 3.26.4+ bug for external thumbnailers
-sudo wget -O /usr/local/bin/bwrap https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/nautilus/bwrap
+sudo wget -O /usr/local/bin/bwrap https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/nautilus/bwrap
 sudo chmod +rx /usr/local/bin/bwrap
 
 # install main script
-sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/apk-thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/apk/apk-thumbnailer
+sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/apk-thumbnailer https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/thumbnailer/apk/apk-thumbnailer
 sudo chmod +x /usr/local/sbin/apk-thumbnailer
 
 # thumbnailer integration
-sudo wget --header='Accept-Encoding:none' -O /usr/share/thumbnailers/apk.thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/apk/apk.thumbnailer
+sudo wget --header='Accept-Encoding:none' -O /usr/share/thumbnailers/apk.thumbnailer https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/thumbnailer/apk/apk.thumbnailer
 
 # stop nautilus
 nautilus -q
