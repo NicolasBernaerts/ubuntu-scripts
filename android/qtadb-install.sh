75ea0e00d4560a544adb5fba0f32292a7cabbae6
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2025-04-30 23:08:19 +0200

    Update qtadb-install.sh

--- android/qtadb-install.sh
+++ android/qtadb-install.sh
@@ -12,12 +12,12 @@ sudo mv ./QtADB*/QtADB /usr/local/sbin/qtadb
 sudo chmod +x /usr/local/sbin/qtadb
 
 # download desktop launcher
-sudo wget --header='Accept-Encoding:none' -O /usr/share/icons/qtadb.png https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/android/qtadb.png
-sudo wget --header='Accept-Encoding:none' -O /usr/share/applications/qtadb.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/android/qtadb.desktop
+sudo wget --header='Accept-Encoding:none' -O /usr/share/icons/qtadb.png https://github.com/NicolasBernaerts/icon/blob/master/qtadb.png
+sudo wget --header='Accept-Encoding:none' -O /usr/share/applications/qtadb.desktop https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/android/qtadb.desktop
 
 # configuration file to get rid of launch crash
 mkdir -p $HOME/.config/Bracia
-wget --header='Accept-Encoding:none' -O $HOME/.config/Bracia/QtADB.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/android/QtADB.conf
+wget --header='Accept-Encoding:none' -O $HOME/.config/Bracia/QtADB.conf https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/android/QtADB.conf
 
 # cleanup
 rm QtADB_0.8.1_linux64.tar.gz
