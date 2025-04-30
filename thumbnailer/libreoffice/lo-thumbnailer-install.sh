d492ac0d086d843727f78d362fe508b9f87571cb
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2025-04-30 22:11:54 +0200

    Update lo-thumbnailer-install.sh

--- thumbnailer/libreoffice/lo-thumbnailer-install.sh
+++ thumbnailer/libreoffice/lo-thumbnailer-install.sh
@@ -10,7 +10,7 @@ sudo apt-get -y install libfile-mimeinfo-perl unzip netpbm
 sudo apt -y install default-jre libreoffice-java-common
 
 # install bubblewrap wrapper to handle Nautilus 3.26.4+ bug for external thumbnailers
-sudo wget -O /usr/local/bin/bwrap https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/nautilus/bwrap
+sudo wget -O /usr/local/bin/bwrap https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/nautilus/bwrap
 sudo chmod +rx /usr/local/bin/bwrap
 
 # create icons ressource directory
@@ -22,7 +22,7 @@ ARR_DOCTYPE=( "database" "graphics" "presentation" "spreadsheet" "text" )
 for DOCTYPE in "${ARR_DOCTYPE[@]}"
 do
 	# download document type icon
-	sudo wget -O "${ROOT_DOCTYPE}/lo-${DOCTYPE}.png" "https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/icons/lo-${DOCTYPE}.png"
+	sudo wget -O "${ROOT_DOCTYPE}/lo-${DOCTYPE}.png" "https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/thumbnailer/libreoffice/icons/lo-${DOCTYPE}.png"
 
 	# generate mask
 	sudo bash -c "pngtopnm ${ROOT_DOCTYPE}/lo-${DOCTYPE}.png | pnmscalefixed -xysize 256 256 - > ${ROOT_DOCTYPE}/lo-${DOCTYPE}.pnm" 
@@ -32,11 +32,11 @@ do
 done
 
 # install main script
-sudo wget -O /usr/local/sbin/lo-thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/lo-thumbnailer
+sudo wget -O /usr/local/sbin/lo-thumbnailer https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/thumbnailer/libreoffice/lo-thumbnailer
 sudo chmod +rx /usr/local/sbin/lo-thumbnailer
 
 # thumbnailer integration
-sudo wget -O /usr/share/thumbnailers/lo.thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/libreoffice/lo.thumbnailer
+sudo wget -O /usr/share/thumbnailers/lo.thumbnailer https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/thumbnailer/libreoffice/lo.thumbnailer
 
 # if present, disable gsf-office.thumbnailer (used in UbuntuGnome 16.04)
 [ -f "/usr/share/thumbnailers/gsf-office.thumbnailer" ] && sudo mv /usr/share/thumbnailers/gsf-office.thumbnailer /usr/share/thumbnailers/gsf-office.thumbnailer.org
