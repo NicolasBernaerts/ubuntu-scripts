52f877435681ef5494ca51635edf6f49b06014c3
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2025-04-30 22:21:25 +0200

    Update pdf-merge-install.sh

--- pdf/pdf-merge-install.sh
+++ pdf/pdf-merge-install.sh
@@ -16,7 +16,7 @@ sudo apt -y install imagemagick unoconv ghostscript
 # if needed, remove imagemagick PDF generation restrictions
 if [ ! -f "/etc/apt/apt.conf.d/99imagemagick-enable-pdf" ]
 then
-	wget https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/image/imagemagisk-enable-pdf-install.sh
+	wget https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/image/imagemagisk-enable-pdf-install.sh
 	if [ -f ./imagemagisk-enable-pdf-install.sh ]
 	then
 		logger "graphical - ImageMagick PDF and PostScript restriction removal"
@@ -30,13 +30,13 @@ fi
 gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "{'Gtk/ButtonImages': <1>, 'Gtk/MenuImages': <1>}"
 
 # install icons
-sudo wget -O /usr/share/icons/pdf-generate.png https://github.com/NicolasBernaerts/icon/raw/master/pdf/pdf-merge.png
+sudo wget -O /usr/share/icons/pdf-generate.png https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/pdf/pdf-merge.png
 
 # main script installation
-sudo wget -O /usr/local/bin/pdf-merge https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-merge
+sudo wget -O /usr/local/bin/pdf-merge https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/pdf/pdf-merge
 sudo chmod +x /usr/local/bin/pdf-merge
 
 # desktop integration
 mkdir --parents $HOME/.local/share/nautilus-python/extensions
-wget -O $HOME/.local/share/nautilus-python/extensions/pdf-merge-menu.py https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/pdf/pdf-merge-menu.py
+wget -O $HOME/.local/share/nautilus-python/extensions/pdf-merge-menu.py https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/pdf/pdf-merge-menu.py
 
