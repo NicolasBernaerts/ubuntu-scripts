a2443df4e60eb33f7c8355e4538bd58bfac57f75
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2025-04-30 23:31:10 +0200

    Update raw-thumbnailer-install.sh

--- thumbnailer/raw/raw-thumbnailer-install.sh
+++ thumbnailer/raw/raw-thumbnailer-install.sh
@@ -5,15 +5,15 @@
 sudo apt-get -y install dcraw libjpeg-turbo-progs netpbm
 
 # install bubblewrap wrapper to handle Nautilus 3.26.4+ bug for external thumbnailers
-sudo wget -O /usr/local/bin/bwrap https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/nautilus/bwrap
+sudo wget -O /usr/local/bin/bwrap https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/nautilus/bwrap
 sudo chmod +rx /usr/local/bin/bwrap
 
 # install main script
-sudo wget -O /usr/local/sbin/raw-thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/raw/raw-thumbnailer
+sudo wget -O /usr/local/sbin/raw-thumbnailer https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/thumbnailer/raw/raw-thumbnailer
 sudo chmod +x /usr/local/sbin/raw-thumbnailer
 
 # thumbnailer integration
-sudo wget -O /usr/share/thumbnailers/raw.thumbnailer https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/thumbnailer/raw/raw.thumbnailer
+sudo wget -O /usr/share/thumbnailers/raw.thumbnailer https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/thumbnailer/raw/raw.thumbnailer
 
 # stop nautilus
 nautilus -q
