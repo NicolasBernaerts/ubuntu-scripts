4ca27ad35fbb3dc0c7b63506405e038fae2d1ce9
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2025-04-30 22:52:06 +0200

    Update image-convert-install.sh

--- image/image-convert-install.sh
+++ image/image-convert-install.sh
@@ -13,9 +13,9 @@ sudo rm --force /usr/share/file-manager/actions/image-convert-*.desktop
 rm --force $HOME/.local/share/file-manager/actions/image-convert-*.desktop
 
 # main script installation
-sudo wget -O /usr/local/bin/image-convert https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/image/image-convert
+sudo wget -O /usr/local/bin/image-convert https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/image/image-convert
 sudo chmod +x /usr/local/bin/image-convert
 
 # desktop integration
 mkdir --parents $HOME/.local/share/nautilus-python/extensions
-wget -O $HOME/.local/share/nautilus-python/extensions/image-convert-menu.py https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/image/image-convert-menu.py
+wget -O $HOME/.local/share/nautilus-python/extensions/image-convert-menu.py https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/image/image-convert-menu.py
