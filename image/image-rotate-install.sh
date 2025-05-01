3e905d0a6e6f9ea713d8dd60e15d18697e3c9b7f
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2025-05-01 09:50:14 +0200

    Update image-rotate-install.sh

--- image/image-rotate-install.sh
+++ image/image-rotate-install.sh
@@ -17,10 +17,10 @@ gsettings set org.gnome.desktop.interface menus-have-icons true
 gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "{'Gtk/ButtonImages': <1>, 'Gtk/MenuImages': <1>}"
 
 # install icons
-sudo wget -O /usr/share/icons/image-rotate.png https://github.com/NicolasBernaerts/icon/blob/master/image/image-rotate.png
-sudo wget -O /usr/share/icons/rotate-left.png https://github.com/NicolasBernaerts/icon/blob/master/rotation/rotate-left.png
-sudo wget -O /usr/share/icons/rotate-right.png https://github.com/NicolasBernaerts/icon/blob/master/rotation/rotate-right.png
-sudo wget -O /usr/share/icons/rotate-updown.png https://github.com/NicolasBernaerts/icon/blob/master/rotation/rotate-updown.png
+sudo wget -O /usr/share/icons/image-rotate.png https://raw.githubusercontent.com/NicolasBernaerts/icon/refs/heads/master/image/image-rotate.png
+sudo wget -O /usr/share/icons/rotate-left.png https://raw.githubusercontent.com/NicolasBernaerts/icon/refs/heads/master/rotation/rotate-left.png
+sudo wget -O /usr/share/icons/rotate-right.png https://raw.githubusercontent.com/NicolasBernaerts/icon/refs/heads/master/rotation/rotate-right.png
+sudo wget -O /usr/share/icons/rotate-updown.png https://raw.githubusercontent.com/NicolasBernaerts/icon/refs/heads/master/rotation/rotate-updown.png
 
 # main script installation
 sudo wget -O /usr/local/bin/image-rotate https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/image/image-rotate
