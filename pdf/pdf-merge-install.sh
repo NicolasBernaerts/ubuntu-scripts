d27451300744eec9e67991efa776ffaf764d482b
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2025-05-01 09:45:23 +0200

    Update pdf-merge-install.sh

--- pdf/pdf-merge-install.sh
+++ pdf/pdf-merge-install.sh
@@ -30,7 +30,7 @@ fi
 gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "{'Gtk/ButtonImages': <1>, 'Gtk/MenuImages': <1>}"
 
 # install icons
-sudo wget -O /usr/share/icons/pdf-generate.png https://github.com/NicolasBernaerts/icon/blob/master/pdf/pdf-merge.png
+sudo wget -O /usr/share/icons/pdf-generate.png https://raw.githubusercontent.com/NicolasBernaerts/icon/refs/heads/master/pdf/pdf-merge.png
 
 # main script installation
 sudo wget -O /usr/local/bin/pdf-merge https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/pdf/pdf-merge
