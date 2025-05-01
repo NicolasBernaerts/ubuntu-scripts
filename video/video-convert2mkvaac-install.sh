153bf625ab5e79b3f51565e514f76ceaf3381f02
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2025-05-01 11:24:59 +0200

    Update video-convert2mkvaac-install.sh

--- video/video-convert2mkvaac-install.sh
+++ video/video-convert2mkvaac-install.sh
@@ -15,16 +15,16 @@ sudo rm --force /usr/share/applications/video-convert2mkvaac.desktop
 rm --force $HOME/.local/share/file-manager/actions/video-convert2mkvaac-action.desktop
 
 # install icons
-sudo wget -O /usr/share/icons/video-aac.png https://github.com/NicolasBernaerts/icon/raw/master/video/video-aac.png
+sudo wget -O /usr/share/icons/video-aac.png https://github.com/NicolasBernaerts/icon/raw/refs/heads/master/video/video-aac.png
 
 # install configuration file
 mkdir --parents $HOME/.config
-wget -O $HOME/.config/video-convert2mkvaac.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/video/video-convert2mkvaac.conf
+wget -O $HOME/.config/video-convert2mkvaac.conf https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/video/video-convert2mkvaac.conf
 
 # install main script
-sudo wget -O /usr/local/bin/video-convert2mkvaac https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/video/video-convert2mkvaac
+sudo wget -O /usr/local/bin/video-convert2mkvaac https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/video/video-convert2mkvaac
 sudo chmod +x /usr/local/bin/video-convert2mkvaac
 
 # declare nautilus menu
 mkdir --parents $HOME/.local/share/nautilus-python/extensions
-wget -O $HOME/.local/share/nautilus-python/extensions/video-convert2mkvaac-menu.py https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/video/video-convert2mkvaac-menu.py
+wget -O $HOME/.local/share/nautilus-python/extensions/video-convert2mkvaac-menu.py https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/video/video-convert2mkvaac-menu.py
