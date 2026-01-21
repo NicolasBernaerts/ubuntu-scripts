e2046d0a705ae8edf91cd811d58e3d2842dcdc86
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2026-01-21 21:10:25 +0100

    Update graphviz-thumbnailer-install.sh

--- thumbnailer/graphviz/graphviz-thumbnailer-install.sh
+++ thumbnailer/graphviz/graphviz-thumbnailer-install.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+# -----------------------------------------
 # Graphviz thumbnailer installation script
+# Should be compatible with 
+#   Ubuntu, Debian and Linux Mint (thanks to kafeenstra)
+# -----------------------------------------
 
 # test Ubuntu distribution
 DISTRO=$(lsb_release -is 2>/dev/null)
-[ "${DISTRO}" != "Ubuntu" ] && { zenity --error --text="This automatic installation script is for Ubuntu only"; exit 1; }
+[ "${DISTRO}" != "Ubuntu" -a "${DISTRO}" != "Debian" -a "${DISTRO}" != "Linuxmint" ] && { zenity --error --text="This  installation script is for Ubuntu, Debian or Linux Mint"; exit 1; }
 
 # install tools
 sudo apt-get -y install graphviz netpbm eog
@@ -28,8 +32,11 @@ sudo chmod +x /usr/local/sbin/graphviz-thumbnailer
 # thumbnailer integration
 sudo wget -O /usr/share/thumbnailers/graphviz.thumbnailer https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/thumbnailer/graphviz/graphviz.thumbnailer
 
-# stop nautilus
-nautilus -q
+# stop file manager
+FILE_MANAGER=$(basename $(which nautilus))
+[ "${FILE_MANAGER}" = "" ] && FILE_MANAGER=$(basename $(which nemo))
+[ "${FILE_MANAGER}" = "" ] && FILE_MANAGER=$(basename $(which dolphin))
+"${FILE_MANAGER}" -q
 
 # empty cache of previous thumbnails
 [ -d "$HOME/.cache/thumbnails" ] && rm --recursive --force $HOME/.cache/thumbnails/*
