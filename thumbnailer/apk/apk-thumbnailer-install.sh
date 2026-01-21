9c5c5e552f3fb32a93ff4137936c0bce445a9650
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2026-01-21 21:23:42 +0100

    Update apk-thumbnailer-install.sh

--- thumbnailer/apk/apk-thumbnailer-install.sh
+++ thumbnailer/apk/apk-thumbnailer-install.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-# APK thumbnailer installation script
+# -----------------------------------------
+# APK files thumbnailer installation script
+# Should be compatible with 
+#   Ubuntu, Debian and Linux Mint (thanks to kafeenstra)
+# -----------------------------------------
 
-# test Ubuntu distribution
+# test distribution
 DISTRO=$(lsb_release -is 2>/dev/null)
-[ "${DISTRO}" != "Ubuntu" ] && { zenity --error --text="This automatic installation script is for Ubuntu only"; exit 1; }
+[ "${DISTRO}" != "Ubuntu" -a "${DISTRO}" != "Debian" -a "${DISTRO}" != "Linuxmint" ] && { zenity --error --text="This installation script is for Ubuntu, Debian or Linux Mint"; exit 1; }
 
 # install tools
 sudo apt-get -y install gvfs-bin aapt unzip netpbm
@@ -19,8 +23,11 @@ sudo chmod +x /usr/local/sbin/apk-thumbnailer
 # thumbnailer integration
 sudo wget -O /usr/share/thumbnailers/apk.thumbnailer https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/thumbnailer/apk/apk.thumbnailer
 
-# stop nautilus
-nautilus -q
+# stop file manager
+FILE_MANAGER=$(basename $(which nautilus))
+[ "${FILE_MANAGER}" = "" ] && FILE_MANAGER=$(basename $(which nemo))
+[ "${FILE_MANAGER}" = "" ] && FILE_MANAGER=$(basename $(which dolphin))
+[ "${FILE_MANAGER}" != "" ] && "${FILE_MANAGER}" -q
 
 # empty cache of previous thumbnails
 [ -d "$HOME/.cache/thumbnails" ] && rm --recursive --force $HOME/.cache/thumbnails/*
