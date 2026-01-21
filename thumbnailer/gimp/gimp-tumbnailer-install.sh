eba421fc92c44c45c2c13c5b4d2bab33ad4b3556
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2026-01-21 21:08:24 +0100

    Update gimp-tumbnailer-install.sh

--- thumbnailer/gimp/gimp-tumbnailer-install.sh
+++ thumbnailer/gimp/gimp-tumbnailer-install.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+# -----------------------------------------
 # GIMP XCF thumbnailer installation script
+# Should be compatible with 
+#   Ubuntu, Debian and Linux Mint (thanks to kafeenstra)
+# -----------------------------------------
 
 # test Ubuntu distribution
 DISTRO=$(lsb_release -is 2>/dev/null)
-[ "${DISTRO}" != "Ubuntu" ] && { zenity --error --text="This automatic installation script is for Ubuntu only"; exit 1; }
+[ "${DISTRO}" != "Ubuntu" -a "${DISTRO}" != "Debian" -a "${DISTRO}" != "Linuxmint" ] && { zenity --error --text="This  installation script is for Ubuntu, Debian or Linux Mint"; exit 1; }
 
 # install tools
 sudo apt -y install netpbm
@@ -19,8 +23,11 @@ sudo chmod +x /usr/local/sbin/gimp-thumbnailer
 # thumbnailer integration
 sudo wget -O /usr/share/thumbnailers/gimp.thumbnailer https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/thumbnailer/gimp/gimp.thumbnailer
 
-# stop nautilus
-nautilus -q
+# stop file manager
+FILE_MANAGER=$(basename $(which nautilus))
+[ "${FILE_MANAGER}" = "" ] && FILE_MANAGER=$(basename $(which nemo))
+[ "${FILE_MANAGER}" = "" ] && FILE_MANAGER=$(basename $(which dolphin))
+"${FILE_MANAGER}" -q
 
 # empty cache of previous thumbnails
 [ -d "$HOME/.cache/thumbnails" ] && rm --recursive --force $HOME/.cache/thumbnails/*
