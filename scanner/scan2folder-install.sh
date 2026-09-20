db3f8d2d9ca09b0467bb424a1769d5604316c79b
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2026-09-20 14:24:24 +0200

    Update scan2folder-install.sh

--- scanner/scan2folder-install.sh
+++ scanner/scan2folder-install.sh
@@ -5,13 +5,13 @@
 sudo apt -y install ghostscript yad hplip-gui
 
 # main script
-sudo wget -O /usr/local/bin/scan2folder https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/scanner/scan2folder
+sudo wget -4 -O /usr/local/bin/scan2folder https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/scanner/scan2folder
 sudo chmod +x /usr/local/bin/scan2folder
 
 # configuration file
 mkdir --parent $HOME/.config
-wget -O $HOME/.config/scan2folder.conf https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/scanner/scan2folder.conf
+wget -4 -O $HOME/.config/scan2folder.conf https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/scanner/scan2folder.conf
 
 # desktop integration
-sudo wget -O /usr/share/applications/scan2folder.desktop https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/scanner/scan2folder.desktop
+sudo wget -4 -O /usr/share/applications/scan2folder.desktop https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/scanner/scan2folder.desktop
 sudo chmod +x /usr/share/applications/scan2folder.desktop
