11ea33bada1acefe01ed90b870a1bd0455104c0b
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2026-09-20 14:36:11 +0200

    Update image-iptc-install.sh

--- image/image-iptc-install.sh
+++ image/image-iptc-install.sh
@@ -5,10 +5,10 @@
 sudo apt-get -y install yad exiv2
 
 # install script
-sudo wget -O /usr/local/bin/image-iptc https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/image/image-iptc
+sudo wget -4 -O /usr/local/bin/image-iptc https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/image/image-iptc
 sudo chmod +x /usr/local/bin/image-iptc
 
 # install desktop environment
-sudo wget -O /usr/share/applications/image-iptc.desktop https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/image/image-iptc.desktop
+sudo wget -4 -O /usr/share/applications/image-iptc.desktop https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/image/image-iptc.desktop
 mkdir --parents $HOME/.local/share/file-manager/actions
-wget --header='Accept-Encoding:none' -O $HOME/.local/share/file-manager/actions/image-iptc-action.desktop https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/image/image-iptc-action.desktop
+wget -4 -O $HOME/.local/share/file-manager/actions/image-iptc-action.desktop https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/image/image-iptc-action.desktop
