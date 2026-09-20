00d6ad4f0474fc9aa8c1dabf977a86bafd95150c
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2026-09-20 14:20:27 +0200

    Update apparmor-bug-install.sh

--- nautilus/apparmor-bug-install.sh
+++ nautilus/apparmor-bug-install.sh
@@ -2,11 +2,11 @@
 # Install service to handle AppArmor bugs
 
 # tool
-sudo wget -O "/usr/local/bin/apparmor-bug" https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/nautilus/apparmor-bug
+sudo wget -4 -O "/usr/local/bin/apparmor-bug" https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/nautilus/apparmor-bug
 sudo chmod +x /usr/local/bin/apparmor-bug
 
 # service
-sudo wget -O "/etc/systemd/system/apparmor-bug.service" https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/nautilus/apparmor-bug.service
+sudo wget -4 -O "/etc/systemd/system/apparmor-bug.service" https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/nautilus/apparmor-bug.service
 sudo chmod 644 /etc/systemd/system/pparmor-bug.service
 sudo systemctl enable apparmor-bug.service
 sudo systemctl start apparmor-bug.service
