decc2087e79b71328e8a47fecbdb95daec12bc20
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2026-09-20 14:35:02 +0200

    Update apparmor-firefox-install.sh

--- image/apparmor-firefox-install.sh
+++ image/apparmor-firefox-install.sh
@@ -2,10 +2,10 @@
 # Re-declare apparmor access after firefox update
 
 # download and run main script
-sudo wget -O /usr/local/bin/apparmor-firefox https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/mozilla/apparmor-firefox
+sudo wget -4 -O /usr/local/bin/apparmor-firefox https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/mozilla/apparmor-firefox
 sudo chmod +x /usr/local/bin/apparmor-firefox
 /usr/local/bin/apparmor-firefox
 
 # apt hook
-sudo wget -O /etc/apt/apt.conf.d/99apparmor-firefox https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/mozilla/99apparmor-firefox
+sudo wget -4 -O /etc/apt/apt.conf.d/99apparmor-firefox https://github.com/NicolasBernaerts/ubuntu-scripts/raw/refs/heads/master/mozilla/99apparmor-firefox
 
