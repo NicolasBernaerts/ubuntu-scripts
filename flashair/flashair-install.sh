6eda4b47f835579349668584ed9c493c3507b3d3
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2026-09-20 14:45:12 +0200

    Update flashair-install.sh

--- flashair/flashair-install.sh
+++ flashair/flashair-install.sh
@@ -15,23 +15,23 @@
 sudo apt install inotify-tools imagemagick curl yad
 
 # install configuration file and set current user
-sudo wget -O /etc/flashair.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/flashair/flashair.conf
+sudo wget -4 -O /etc/flashair.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/flashair/flashair.conf
 
 # download icons
-sudo wget -O /usr/share/icons/flashair-icon.png https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/flashair/icons/flashair-icon.png
-sudo wget -O /usr/share/icons/flashair-download.png https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/flashair/icons/flashair-download.png
-sudo wget -O /usr/share/icons/flashair-upload.png https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/flashair/icons/flashair-upload.png
-sudo wget -O /usr/share/icons/flashair-delete.png https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/flashair/icons/flashair-delete.png
-sudo wget -O /usr/share/icons/flashair-size.png https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/flashair/icons/flashair-size.png
-sudo wget -O /usr/share/icons/flashair-type-generic.jpg https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/flashair/icons/flashair-type-generic.jpg
-sudo wget -O /usr/share/icons/flashair-type-photo.jpg https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/flashair/icons/flashair-type-photo.jpg
-sudo wget -O /usr/share/icons/flashair-type-video.jpg https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/flashair/icons/flashair-type-video.jpg
+sudo wget -4 -O /usr/share/icons/flashair-icon.png https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/flashair/icons/flashair-icon.png
+sudo wget -4 -O /usr/share/icons/flashair-download.png https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/flashair/icons/flashair-download.png
+sudo wget -4 -O /usr/share/icons/flashair-upload.png https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/flashair/icons/flashair-upload.png
+sudo wget -4 -O /usr/share/icons/flashair-delete.png https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/flashair/icons/flashair-delete.png
+sudo wget -4 -O /usr/share/icons/flashair-size.png https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/flashair/icons/flashair-size.png
+sudo wget -4 -O /usr/share/icons/flashair-type-generic.jpg https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/flashair/icons/flashair-type-generic.jpg
+sudo wget -4 -O /usr/share/icons/flashair-type-photo.jpg https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/flashair/icons/flashair-type-photo.jpg
+sudo wget -4 -O /usr/share/icons/flashair-type-video.jpg https://github.com/NicolasBernaerts/ubuntu-scripts/raw/master/flashair/icons/flashair-type-video.jpg
 
 # download main scripts
-sudo wget -O /usr/local/sbin/flashair-common https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/flashair/flashair-common
-sudo wget -O /usr/local/sbin/flashair-daemon https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/flashair/flashair-daemon
-sudo wget -O /usr/local/sbin/flashair-command https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/flashair/flashair-command
-sudo wget -O /usr/local/sbin/flashair-network https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/flashair/flashair-network
+sudo wget -4 -O /usr/local/sbin/flashair-common https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/flashair/flashair-common
+sudo wget -4 -O /usr/local/sbin/flashair-daemon https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/flashair/flashair-daemon
+sudo wget -4 -O /usr/local/sbin/flashair-command https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/flashair/flashair-command
+sudo wget -4 -O /usr/local/sbin/flashair-network https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/flashair/flashair-network
 sudo chmod +x /usr/local/sbin/flashair-common /usr/local/sbin/flashair-daemon /usr/local/sbin/flashair-command /usr/local/sbin/flashair-network
 
 # install network setup script
@@ -40,9 +40,9 @@ sudo ln -s /usr/local/sbin/flashair-network /etc/network/if-post-down.d/flashair
 
 # declare nautilus action
 sudo mkdir --parents /usr/local/share/file-manager/actions
-sudo wget -O /usr/local/share/file-manager/actions/flashair-action-download.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/flashair/flashair-action-download.desktop
-sudo wget -O /usr/local/share/file-manager/actions/flashair-action-upload.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/flashair/flashair-action-upload.desktop
-sudo wget -O /usr/local/share/file-manager/actions/flashair-action-delete.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/flashair/flashair-action-delete.desktop
+sudo wget -4 -O /usr/local/share/file-manager/actions/flashair-action-download.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/flashair/flashair-action-download.desktop
+sudo wget -4 -O /usr/local/share/file-manager/actions/flashair-action-upload.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/flashair/flashair-action-upload.desktop
+sudo wget -4 -O /usr/local/share/file-manager/actions/flashair-action-delete.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/flashair/flashair-action-delete.desktop
 
 # restart networking services
 sudo service networking restart
