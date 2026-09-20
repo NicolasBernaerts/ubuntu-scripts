5ac08cca6389dc252a583e2c108bd0ce5e960b64
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2026-09-20 14:43:41 +0200

    Update video-stabilize-install.sh

--- video/video-stabilize-install.sh
+++ video/video-stabilize-install.sh
@@ -19,7 +19,7 @@ command -v nautilus >/dev/null 2>&1 && sudo apt-get -y install nautilus-actions
 # If not present, install specific version of melt with vid.stab filter
 if [ ! -f /opt/vidstab/melt ]
 then
-  wget --header='Accept-Encoding:none' https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/video/melt-vidstab-install.sh
+  wget -4 https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/video/melt-vidstab-install.sh
   chmod +x melt-vidstab-install.sh
   ./melt-vidstab-install.sh
   rm melt-vidstab-install.sh
@@ -27,18 +27,18 @@ fi
 
 # install configuration file
 mkdir --parents $HOME/.config
-wget --header='Accept-Encoding:none' -O $HOME/.config/video-stabilize.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/video/video-stabilize.conf
+wget -4 -O $HOME/.config/video-stabilize.conf https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/video/video-stabilize.conf
 
 # install main script
-sudo wget --header='Accept-Encoding:none' -O /usr/local/bin/video-stabilize https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/video/video-stabilize
+sudo wget -4 -O /usr/local/bin/video-stabilize https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/video/video-stabilize
 sudo chmod +x /usr/local/bin/video-stabilize
 
 # desktop integration : icon
-sudo wget --header='Accept-Encoding:none' -O /usr/share/icons/stabilizer.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/video/stabilizer.png
+sudo wget -4 -O /usr/share/icons/stabilizer.png https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/video/stabilizer.png
 
 # desktop integration : application
-sudo wget --header='Accept-Encoding:none' -O /usr/share/applications/video-stabilize.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/video/video-stabilize.desktop
+sudo wget -4 -O /usr/share/applications/video-stabilize.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/video/video-stabilize.desktop
 
 # desktop integration : nautilus action
 mkdir --parents $HOME/.local/share/file-manager/actions
-wget --header='Accept-Encoding:none' -O $HOME/.local/share/file-manager/actions/video-stabilize-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/video/video-stabilize-action.desktop
+wget -4 -O $HOME/.local/share/file-manager/actions/video-stabilize-action.desktop https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/video/video-stabilize-action.desktop
